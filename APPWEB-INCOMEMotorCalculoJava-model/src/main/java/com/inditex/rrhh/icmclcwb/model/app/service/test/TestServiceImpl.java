c0a42531045342c9f1037174cc33479be42d0db2
David Tenreiro Souto <davidtso@ext.inditex.com>
2018-11-12 16:03:58 +0100

    > Control de errores

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/service/test/TestServiceImpl.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/service/test/TestServiceImpl.java
@@ -1,13 +1,17 @@
 package com.inditex.rrhh.icmclcwb.model.app.service.test;
 
+import com.inditex.aqsw.framework.common.core.exception.ApplicationException;
 import com.inditex.aqsw.framework.service.aaa.classic.serviciossso.UserSSO;
 import com.inditex.aqsw.framework.service.aaa.classic.util.SsoUtils;
 import com.inditex.rrhh.icmclcwb.api.app.dto.test.RelojDto;
 import com.inditex.rrhh.icmclcwb.api.app.dto.test.SsoDto;
 import com.inditex.rrhh.icmclcwb.api.app.service.test.TestService;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
@@ -30,6 +34,17 @@ public class TestServiceImpl implements TestService {
 		LOG.info("userSso: {}", userSso);
 		result.setResult(userSso.toString());
 		return result;
+	}
+
+	@Override
+	public void errorSync() throws Exception {
+		throw new ApplicationException("Synchronous error");
+	}
+
+	@Async
+	@Override
+	public CompletableFuture<Void> errorAsync() throws Exception {
+		throw new ApplicationException("Asynchronous error");
 	}
 
 }
