3c4019990cf04b520004ad1cc12826879f86ecc4
David Tenreiro Souto <davidtso@ext.inditex.com>
2018-11-14 17:58:30 +0100

    > Sesión de meta4 por clase y no por anotación

--- APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/aop/meta4/login/Meta4SessionAspect.java
+++ APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/aop/meta4/login/Meta4SessionAspect.java
@@ -2,7 +2,6 @@ package com.inditex.rrhh.icmclcwb.aop.meta4.login;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,20 +30,16 @@ public class Meta4SessionAspect {
 	@Value("${app.envars.meta4.language}")
 	private String language;
 
-	@Pointcut("@annotation(com.inditex.rrhh.icmclcwb.api.meta4.util.annotation.Meta4Session)")
-	public void meta4SessionAnnotationPointCutDefinition() {
-	}
-
-	@Before("meta4SessionAnnotationPointCutDefinition()")
-	public void beforeMeta4SessionAnnotationPointCutDefinition() throws Throwable {
-		LOG.info("Inicio :: Meta4SessionAspect.beforeMeta4SessionAnnotationPointCutDefinition()");
-		if (meta4LoginService.retrieveM4Session() || meta4LoginService.login(new LoginRequestDto(user, password, language))) {
-			LOG.info("Meta4SessionAspect.beforeMeta4SessionAnnotationPointCutDefinition(): Login = OK");
-		} else {
-			LOG.error("Meta4SessionAspect.beforeMeta4SessionAnnotationPointCutDefinition(): Login = KO");
-			throw new ApplicationException("No se puede invocar un servicio Meta4 sin tener sessionID valido");
-		}
-		LOG.info("Fin :: Meta4SessionAspect.beforeMeta4SessionAnnotationPointCutDefinition()");
+	@Before(value = "execution(public * com.inditex.rrhh.icmclcwb.api.meta4.service.Meta4SessionService.*(..))")
+	public void beforeMeta4SessionAnnotationPointCutDefinition() throws Exception {
+	  LOG.info("Inicio :: Meta4SessionAspect.beforeMeta4SessionAnnotationPointCutDefinition()");
+      if (meta4LoginService.retrieveM4Session() || meta4LoginService.login(new LoginRequestDto(user, password, language))) {
+          LOG.info("Meta4SessionAspect.beforeMeta4SessionAnnotationPointCutDefinition(): Login = OK");
+      } else {
+          LOG.error("Meta4SessionAspect.beforeMeta4SessionAnnotationPointCutDefinition(): Login = KO");
+          throw new ApplicationException("No se puede invocar un servicio Meta4 sin tener sessionID valido");
+      }
+      LOG.info("Fin :: Meta4SessionAspect.beforeMeta4SessionAnnotationPointCutDefinition()");
 	}
 
 }
