6bdd978640415b7cf0848e184be5bd5c1466e376
David Yáñez Garcia <dyanez@vectoritcgroup.com>
2018-11-15 18:08:13 +0100

    calculo

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/repository/GTCalculoRepositoryImpl.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/repository/GTCalculoRepositoryImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.inditex.rrhh.icmclcwb.model.primary.domain.CalculoCriteria;
+
 @Repository
 public class GTCalculoRepositoryImpl  implements GTCalculoRepository{
 
@@ -15,8 +17,16 @@ public class GTCalculoRepositoryImpl  implements GTCalculoRepository{
 	@Autowired
 	@Qualifier("primaryJdbcTemplate")
 	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public boolean calcular(CalculoCriteria criteria) {				
+		
+		
+		return false;
+	}
 	
 	
+
 	
 	
 }
