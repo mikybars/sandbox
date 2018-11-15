9d059ae4518e19173a1d96e430363077bb616ffd
David Yáñez Garcia <dyanez@vectoritcgroup.com>
2018-11-15 18:57:37 +0100

    calculo

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/repository/GTCalculoRepository.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/repository/GTCalculoRepository.java
@@ -1,5 +1,9 @@
 package com.inditex.rrhh.icmclcwb.model.primary.repository;
 
+import com.inditex.rrhh.icmclcwb.model.primary.domain.CalculoCriteria;
+
 public interface GTCalculoRepository {
+		
+	public boolean calcular(CalculoCriteria criteria);
 
 }
