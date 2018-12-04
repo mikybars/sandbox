143dac36a509849b7873dbb4c912750a447baa7f
David Tenreiro Souto <davidtso@ext.inditex.com>
2018-12-04 16:31:33 +0100

    > Exclude fields toString

--- APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/dto/ProgramacionEmpleadoDto.java
+++ APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/dto/ProgramacionEmpleadoDto.java
@@ -1,6 +1,7 @@
 package com.inditex.rrhh.icmclcwb.api.app.dto;
 
 import lombok.Data;
+import lombok.ToString;
 
 import java.io.Serializable;
 
@@ -13,9 +14,11 @@ public class ProgramacionEmpleadoDto implements Serializable {
 
 	private static final long serialVersionUID = -5607507260079003307L;
 
+	@ToString.Exclude
 	@ApiModelProperty(value = "Identificador de la tienda", required = false, readOnly = true, hidden = true)
 	private String id;
 
+	@ToString.Exclude
 	@ApiModelProperty(value = "Programación", required = false, readOnly = true, hidden = true)
 	private ProgramacionDto programacion;
 	
