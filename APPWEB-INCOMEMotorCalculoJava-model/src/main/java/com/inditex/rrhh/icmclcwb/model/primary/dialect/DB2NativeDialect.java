438b4a409f7bac32420479f77a222b438f16d99c
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-01-24 14:32:21 +0100

    > DECIMAL(23,8)

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/dialect/DB2NativeDialect.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/dialect/DB2NativeDialect.java
@@ -1,7 +1,17 @@
 package com.inditex.rrhh.icmclcwb.model.primary.dialect;
 
+import java.sql.Types;
+
 import org.hibernate.dialect.DB2Dialect;
 
 public class DB2NativeDialect extends DB2Dialect {
+
+    public DB2NativeDialect() {
+        super();
+        registerColumnType(Types.DOUBLE, "decimal(23,8)");
+//        registerColumnType(Types.INTEGER, "decimal($p)");
+//        registerColumnType(Types.NUMERIC, "decimal($p,$s)");
+//        registerColumnType(Types.DECIMAL, "decimal($p,$s)");
+    }
 
 }
