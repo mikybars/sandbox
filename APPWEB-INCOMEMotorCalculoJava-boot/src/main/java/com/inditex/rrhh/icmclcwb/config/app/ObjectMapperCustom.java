a13bd7f4177215e23fcf4bfd02d3525c3a4adf41
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-01-30 17:33:05 +0100

    > JAutodoc

--- APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/ObjectMapperCustom.java
+++ APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/ObjectMapperCustom.java
@@ -7,10 +7,17 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
+/**
+ * The Class ObjectMapperCustom.
+ */
 public class ObjectMapperCustom extends ObjectMapper {
 
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = -8640529927698069606L;
 
+    /**
+     * Instantiates a new object mapper custom.
+     */
     public ObjectMapperCustom() {
         super();
         this.registerModule(new ParameterNamesModule());
@@ -19,11 +26,24 @@ public class ObjectMapperCustom extends ObjectMapper {
         this.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     }
 
+    /**
+     * Register module.
+     *
+     * @param module the module
+     * @return the object mapper
+     */
     @Override
     public final ObjectMapper registerModule(Module module) {
         return super.registerModule(module);
     }
 
+    /**
+     * Configure.
+     *
+     * @param f the f
+     * @param state the state
+     * @return the object mapper
+     */
     @Override
     public final ObjectMapper configure(SerializationFeature f, boolean state) {
         return super.configure(f, state);
--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/secondary/dialect/SQLServerNativeDialect.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/secondary/dialect/SQLServerNativeDialect.java
@@ -4,14 +4,26 @@ import java.sql.Types;
 
 import org.hibernate.dialect.SQLServerDialect;
 
+/**
+ * The Class SQLServerNativeDialect.
+ */
 public class SQLServerNativeDialect extends SQLServerDialect {
 
+    /**
+     * Instantiates a new SQL server native dialect.
+     */
     public SQLServerNativeDialect() {
         super();
         this.registerColumnType(Types.VARCHAR, "nvarchar($l)");
         this.registerColumnType(Types.CLOB, "ntext");
     }
 
+    /**
+     * Register column type.
+     *
+     * @param code the code
+     * @param name the name
+     */
     @Override
     public final void registerColumnType(int code, String name) {
         super.registerColumnType(code, name);
