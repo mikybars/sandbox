8ca85432423a81d6c02a90d126bc82d3660579f5
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-01-30 16:36:40 +0100

    > Sonar

--- APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/ObjectMapperCustom.java
+++ APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/ObjectMapperCustom.java
@@ -1,5 +1,6 @@
 package com.inditex.rrhh.icmclcwb.config.app;
 
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -7,23 +8,25 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 public class ObjectMapperCustom extends ObjectMapper {
-	
-	private static final long serialVersionUID = -8640529927698069606L;
 
-	public ObjectMapperCustom() {
-		super();
-		registerModule(new ParameterNamesModule());
-		registerModule(new Jdk8Module());
-		JavaTimeModule timeModule = new JavaTimeModule();
-//		timeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
-//		timeModule.addSerializer(LocalDateTime.class,
-//				new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-//		timeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ISO_LOCAL_DATE));
-//		timeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-		registerModule(timeModule);
-		configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-	}
+    private static final long serialVersionUID = -8640529927698069606L;
 
+    public ObjectMapperCustom() {
+        super();
+        this.registerModule(new ParameterNamesModule());
+        this.registerModule(new Jdk8Module());
+        this.registerModule(new JavaTimeModule());
+        this.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
 
+    @Override
+    public final ObjectMapper registerModule(Module module) {
+        return super.registerModule(module);
+    }
+
+    @Override
+    public final ObjectMapper configure(SerializationFeature f, boolean state) {
+        return super.configure(f, state);
+    }
 
 }
