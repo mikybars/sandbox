0ceec1c89e80c45acfc1d01141089d35c3af36b6
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-01-30 16:09:44 +0100

    > Dialect

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/secondary/dialect/SQLServerNativeDialect.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/secondary/dialect/SQLServerNativeDialect.java
@@ -6,20 +6,9 @@ import org.hibernate.dialect.SQLServerDialect;
 
 public class SQLServerNativeDialect extends SQLServerDialect {
 
-	public SQLServerNativeDialect() {
-		super();
-//		registerColumnType(Types.CHAR, "nchar(1)");
-	    registerColumnType(Types.VARCHAR, "nvarchar($l)");
-//	    registerColumnType(Types.LONGVARCHAR, "nvarchar($l)");
-	    registerColumnType(Types.CLOB, "ntext");
-	}
-
-//	public String getTypeName(int code, int length, int precision, int scale) throws HibernateException {
-//		if (code != 2005) {
-//			return super.getTypeName(code, length, precision, scale);
-//		} else {
-//			return "ntext";
-//		}
-//	}
+    public SQLServerNativeDialect() {
+        registerColumnType(Types.VARCHAR, "nvarchar($l)");
+        registerColumnType(Types.CLOB, "ntext");
+    }
 
 }
