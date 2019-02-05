697615a7b7e21dc1f921510f849472be4f5f0978
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-02-05 18:32:07 +0100

    > Consulta cálculo

--- APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/trabajo/CalculoFactoryConfig.java
+++ APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/trabajo/CalculoFactoryConfig.java
@@ -1,18 +1,13 @@
 package com.inditex.rrhh.icmclcwb.config.app.trabajo;
 
 import org.springframework.beans.factory.FactoryBean;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-
 import com.inditex.rrhh.icmclcwb.model.app.trabajo.CalculoAlgoritmoFactory;
 import com.inditex.rrhh.icmclcwb.model.app.trabajo.GTAlgoritmo;
 
 @Configuration
-@ComponentScan(basePackages = { "com.inditex.rrhh.icmclcwb.model.app.calculo.fac" })
 public class CalculoFactoryConfig {
 
     @Bean
@@ -23,7 +18,6 @@ public class CalculoFactoryConfig {
     }
 
     @Bean(name = "gTAlgoritmo")
-    @Scope(scopeName = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public GTAlgoritmo gTAlgoritmo() {
         return new GTAlgoritmo();
     }
