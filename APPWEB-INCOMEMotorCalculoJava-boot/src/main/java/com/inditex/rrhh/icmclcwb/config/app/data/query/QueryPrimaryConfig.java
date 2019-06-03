a3bbf8718cea30bdf42b2f799d62b25414d02a59
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-06-03 09:38:17 +0200

    > Cast consultas

--- APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/data/query/QueryPrimaryConfig.java
+++ APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/data/query/QueryPrimaryConfig.java
@@ -14,65 +14,60 @@ import org.springframework.core.io.support.ResourcePatternUtils;
 
 @Configuration
 public class QueryPrimaryConfig {
-    
-    @Value("${app.envars.calculo.precision}")
-    private String precisionCalculo;
-    
+
     @Value("${app.envars.calculo.cast}")
     private boolean castCalculo;
-    
-    @Value("${app.envars.proceso.precision}")
-    private String precisionProceso;
-    
+
+    @Value("AS ${app.envars.calculo.precision})")
+    private String precisionCalculo;
+
     @Value("${app.envars.proceso.cast}")
     private boolean castProceso;
-    
+
+    @Value("${app.envars.proceso.precision}")
+    private String precisionProceso;
+
     private static final String RESOURCE_COMMON = "classpath*:/query/primary/*.xml";
     private static final String RESOURCE_CALCULO = "classpath*:/query/primary/calculo/*.xml";
-    private static final String PRECISION_CONSTANT_INICIO = "/*PRECISION_INI*/";
-    private static final String PRECISION_CONSTANT_FIN = "/*PRECISION_FIN*/";
-    private static final String PRECISION_TYPE = "/*PRECISION*/";
-    private static final String CAST_INI = "CAST(";
-    private static final String CAST_END = ")";
+    private static final String CAST_REPLACE_INICIO = "/*(*/";
+    private static final String CAST_REPLACE_FIN = "/*)*/";
+    private static final String CAST_RESULT_INICIO = "CAST(";
+    private static final String CAST_RESULT_FIN = "AS ${precision})";
+    private static final String CAST_VAR_PRECISION = "${precision}";
 
     @Bean(name = "primaryQuery")
-    public PropertiesFactoryBean primaryQuery(ResourceLoader resourceLoader) throws IOException {
+    public PropertiesFactoryBean primaryQuery(final ResourceLoader resourceLoader) throws IOException {
         return loadBean(resourceLoader, RESOURCE_COMMON, precisionProceso, castProceso);
     }
-    
+
     @Bean(name = "calculoPrimaryQuery")
-    public PropertiesFactoryBean calculoPrimaryQuery(ResourceLoader resourceLoader) throws IOException {
+    public PropertiesFactoryBean calculoPrimaryQuery(final ResourceLoader resourceLoader) throws IOException {
         return loadBean(resourceLoader, RESOURCE_CALCULO, precisionCalculo, castCalculo);
     }
 
-    private PropertiesFactoryBean loadBean(ResourceLoader resourceLoader, String resource, String precision,
-            boolean cast) throws IOException {
+    private PropertiesFactoryBean loadBean(final ResourceLoader resourceLoader, final String resource,
+            final String precision, final boolean cast) throws IOException {
         PropertiesFactoryBean bean = new PropertiesFactoryBean();
-        Resource[] resources = ResourcePatternUtils.getResourcePatternResolver(resourceLoader)
-                .getResources(resource);
+        Resource[] resources = ResourcePatternUtils.getResourcePatternResolver(resourceLoader).getResources(resource);
         bean.setLocations(resources);
         bean.setIgnoreResourceNotFound(true);
         bean.afterPropertiesSet();
         Properties props = bean.getObject();
-        
-        for(Object key: props.keySet()){
+        String castInicio = StringUtils.EMPTY;
+        String castFin = StringUtils.EMPTY;
+        if (cast) {
+            castInicio = CAST_RESULT_INICIO;
+            castFin = CAST_RESULT_FIN.replace(CAST_VAR_PRECISION, precision);
+        }
+        for (Object key : props.keySet()) {
             String prop = (String) props.get(key);
-            if (cast) {
-                prop = prop.replace(PRECISION_CONSTANT_INICIO, CAST_INI);
-                prop = prop.replace(PRECISION_CONSTANT_FIN, CAST_END);
-                prop = prop.replace(PRECISION_TYPE, precision);
-            }else {
-                prop = prop.replace(PRECISION_CONSTANT_INICIO, StringUtils.EMPTY);
-                prop = prop.replace(PRECISION_CONSTANT_FIN, StringUtils.EMPTY);
-                prop = prop.replace(PRECISION_TYPE, StringUtils.EMPTY);
-            }
+            prop = prop.replace(CAST_REPLACE_INICIO, castInicio);
+            prop = prop.replace(CAST_REPLACE_FIN, castFin);
             props.put(key, prop);
         }
-        
         bean.setLocalOverride(true);
         bean.setProperties(props);
         bean.afterPropertiesSet();
-
         return bean;
     }
 
