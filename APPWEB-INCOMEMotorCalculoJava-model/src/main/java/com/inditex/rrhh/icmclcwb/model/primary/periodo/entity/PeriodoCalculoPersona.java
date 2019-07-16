458c4b0cef27b51bcb4ff7af9ab54dedb05cadfd
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-07-16 13:07:19 +0200

    > Periodo

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/periodo/entity/PeriodoCalculoPersona.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/periodo/entity/PeriodoCalculoPersona.java
@@ -21,27 +21,29 @@ public class PeriodoCalculoPersona {
 
     @EmbeddedId
     private PeriodoCalculoPersonaPk pk;
-    
+
     @NotNull
     @OneToOne
     @JoinColumn(name = "ID_TAREA_ACTUAL", nullable = false)
     private /* BigInteger */ Tarea tareaActual;
-    
+
     @NotNull
     @OneToOne
     @JoinColumn(name = "ID_TAREA_ULTIMA", nullable = false)
     private /* BigInteger */ Tarea tareaUltima;
-      
-    @Column(name = "FECHA_CREACION", nullable = true)
+
+    @NotNull
+    @Column(name = "FECHA_CREACION", nullable = false)
     private Date fechaCreacion;
 
-    @Column(name = "FECHA_ACTUALIZACION", nullable = true)
+    @NotNull
+    @Column(name = "FECHA_ACTUALIZACION", nullable = false)
     private Date fechaActualizacion;
-    
+
     @OneToOne
     @JoinColumn(name = "ID_ESTADO", nullable = false)
     private EstadoPeriodoCalculoPersona estado;
-    
+
     @NotNull
     @Column(name = "BLOQUEADO", nullable = false)
     private Boolean bloqueado;
