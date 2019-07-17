288d0ab63d502bf3ae6403528f825142ccd61979
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-07-17 13:53:37 +0200

    > Resilience4j

--- APPWEB-INCOMEMotorCalculoJava-ms/src/main/java/com/inditex/rrhh/icmclcwb/ms/app/tarea/ReceiverTarea.java
+++ APPWEB-INCOMEMotorCalculoJava-ms/src/main/java/com/inditex/rrhh/icmclcwb/ms/app/tarea/ReceiverTarea.java
@@ -3,11 +3,14 @@ package com.inditex.rrhh.icmclcwb.ms.app.tarea;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.Message;
+
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.inditex.rrhh.icmclcwb.api.app.run.service.RunService;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @Component
 public class ReceiverTarea {
@@ -15,6 +18,7 @@ public class ReceiverTarea {
     @Autowired
     private RunService runService;
 
+    @CircuitBreaker(name = "tarea")
     @Transactional
     @JmsListener(id = "tareaListener", destination = "${amiga.service.jms.tarea-queue.destination-fqdn}", containerFactory = "tareaContainerFactoryListener")
     public void onMessageTareaListener(
