5bbf7e68de8b0831f84e3ffcd1a121a0764229a3
Javier Estévez <jestevez@vectoritcgroup.com>
2019-08-16 13:26:34 +0200

    Tests sobre repositorios de TareaAgrupacion*, TareaAmbitoGlobalLocalizacionPersonaServiceImpl, TareaaAmbitoGlobalLocalizacionPersonaRepositoryCustomImpl

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/tarea/service/TareaAmbitoGlobalLocalizacionPersonaServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.inditex.rrhh.icmclcwb.model.app.tarea.service;
+
+import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
+import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaAmbitoGlobalLocalizacionPersonaRepositoryCustom;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TareaAmbitoGlobalLocalizacionPersonaServiceImplTest {
+
+    @Mock
+    private TareaAmbitoGlobalLocalizacionPersonaRepositoryCustom tareaAmbitoGlobalLocalizacionPersonaRepositoryCustom;
+
+    @InjectMocks
+    private TareaAmbitoGlobalLocalizacionPersonaServiceImpl tareaAmbitoGlobalLocalizacionPersonaService;
+
+    @Test
+    public void mergePersonaLocalizacionTest() {
+
+        RunTareaDto runTareaDto = mock(RunTareaDto.class);
+        tareaAmbitoGlobalLocalizacionPersonaService.mergePersonaLocalizacion(runTareaDto);
+
+        verify(tareaAmbitoGlobalLocalizacionPersonaRepositoryCustom, times(1)).mergePersonaLocalizacion(any(RunTareaDto.class));
+
+    }
+
+}
