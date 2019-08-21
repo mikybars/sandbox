2bde9bfd69507928ae0edc61671dde5c974030bb
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-08-21 14:23:08 +0200

    > Log carga DDL

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/service/PrimaryServiceImpl.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/service/PrimaryServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
@@ -19,15 +20,25 @@ import com.inditex.rrhh.icmclcwb.model.primary.repository.PrimaryRepositoryCusto
 public class PrimaryServiceImpl implements PrimaryService {
 
     @Autowired
+    private Logger log;
+    
+    @Autowired
     private PrimaryRepositoryCustom primaryRepositoryCustom;
 
     @Override
     public Boolean loadDML(@Valid @NotBlank final String path) {
+        Boolean result = Boolean.FALSE;
+        log.info("Inicio carga path {}", path);
         ClassPathResource resource = new ClassPathResource(path);
+        log.info("Fin carga path {}", path);
         if (!resource.exists()) {
-            throw new IcmclcwbException("No existe el path");
+            log.error("No existe el path {}", path);
+            throw new IcmclcwbException("No existe el path " + path);
         }
-        return primaryRepositoryCustom.load(Arrays.asList(resource));
+        log.info("Inicio load path {}", path);
+        result = primaryRepositoryCustom.load(Arrays.asList(resource));
+        log.info("Fin load path {} con resultado {}", path, result);
+        return result;
     }
 
 }
