1ca3887588dfd7f78116040a5e8b9963b76e99f7
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-08-22 14:19:30 +0200

    > Revert force error programacion

--- APPWEB-INCOMEMotorCalculoJava-ws/src/main/java/com/inditex/rrhh/icmclcwb/ws/app/run/programacion/controller/RunProgramacionController.java
+++ APPWEB-INCOMEMotorCalculoJava-ws/src/main/java/com/inditex/rrhh/icmclcwb/ws/app/run/programacion/controller/RunProgramacionController.java
@@ -2,7 +2,6 @@ package com.inditex.rrhh.icmclcwb.ws.app.run.programacion.controller;
 
 import java.util.List;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.inditex.rrhh.icmclcwb.api.app.exception.IcmclcwbException;
 import com.inditex.rrhh.icmclcwb.api.app.run.programacion.dto.RunProgramacionDto;
 import com.inditex.rrhh.icmclcwb.api.app.run.programacion.service.RunProgramacionService;
 import io.swagger.annotations.Api;
@@ -30,11 +28,7 @@ public class RunProgramacionController {
 	@PreAuthorize("hasAuthority('admin')")
 	@ApiOperation("Revisa si hay programaciones pendientes de lanzar y en caso afirmativo genera las acciones necesarias")
 	public List<RunProgramacionDto> run() {
-	    List<RunProgramacionDto> result = runProgramacionService.run();
-	    if(CollectionUtils.isEmpty(result)) {
-	        throw new IcmclcwbException("Se han procesado cálculos, esto es un mock para validar la planificación de control-m");
-	    }
-	    return result;
+		return runProgramacionService.run();
 	}
 
 }
