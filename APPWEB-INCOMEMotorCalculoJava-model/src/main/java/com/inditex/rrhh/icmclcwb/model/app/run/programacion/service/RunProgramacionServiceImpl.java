693ed2a5b48b1ee10b62f8e38606af33249c97ab
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-08-23 13:56:01 +0200

    > Error SQL activación programaciones

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/run/programacion/service/RunProgramacionServiceImpl.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/run/programacion/service/RunProgramacionServiceImpl.java
@@ -2,6 +2,9 @@ package com.inditex.rrhh.icmclcwb.model.app.run.programacion.service;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +18,7 @@ import com.inditex.rrhh.icmclcwb.api.app.trabajo.service.TrabajoService;
 import com.inditex.rrhh.icmclcwb.api.meta4.dto.PageDto;
 import com.inditex.rrhh.icmclcwb.api.meta4.icmwscalcincome.generic.dto.GenericFilterDto;
 import com.inditex.rrhh.icmclcwb.api.meta4.icmwscalcincome.generic.dto.GenericFilterParametersDto;
+import com.inditex.rrhh.icmclcwb.api.meta4.icmwscalcincome.periodos.dto.PeriodoDto;
 import com.inditex.rrhh.icmclcwb.api.meta4.icmwscalcincome.periodos.dto.PeriodosRequestDto;
 import com.inditex.rrhh.icmclcwb.api.meta4.icmwscalcincome.service.Meta4IcmWsCalcIncomeSessionService;
 import com.inditex.rrhh.icmclcwb.model.app.periodo.mapper.PeriodoMapper;
@@ -22,6 +26,9 @@ import com.inditex.rrhh.icmclcwb.model.app.periodo.mapper.PeriodoMapper;
 @Service
 @Validated
 public class RunProgramacionServiceImpl implements RunProgramacionService {
+
+    @Autowired
+    private Logger log;
 
     @Autowired
     private ProgramacionService programacionService;
@@ -52,14 +59,20 @@ public class RunProgramacionServiceImpl implements RunProgramacionService {
                         .add(GenericFilterParametersDto.builder().idSociedadReg(programacionAmbito.getIdOrgenization())
                                 .abierto(Boolean.TRUE.toString()).activo(Boolean.TRUE.toString())
                                 .vigente(Boolean.TRUE.toString()).build());
-                periodoMapper.periodoResultItemDtoToPeriodoDto(meta4IcmWsCalcIncomeSessionService.getPeriodos(request))
-                        .stream()
-                        .forEach(periodo -> runProgramacion.getRunProgramacionPeriodo()
-                                .add(RunProgramacionPeriodoDto.builder().periodo(periodo)
-                                        .programacionAmbito(programacionAmbito)
-                                        .trabajo(trabajoService.create(
-                                                trabajoService.merge(programacion, programacionAmbito, periodo)))
-                                        .build()));
+                List<PeriodoDto> periodos = periodoMapper
+                        .periodoResultItemDtoToPeriodoDto(meta4IcmWsCalcIncomeSessionService.getPeriodos(request));
+                if (CollectionUtils.isNotEmpty(periodos)) {
+                    periodos.stream()
+                            .forEach(periodo -> runProgramacion.getRunProgramacionPeriodo()
+                                    .add(RunProgramacionPeriodoDto.builder().periodo(periodo)
+                                            .programacionAmbito(programacionAmbito)
+                                            .trabajo(trabajoService.create(
+                                                    trabajoService.merge(programacion, programacionAmbito, periodo)))
+                                            .build()));
+                } else {
+                    log.warn("No existen periodos activos para la organización {}",
+                            programacionAmbito.getIdOrgenization());
+                }
             });
             result.add(runProgramacion);
         });
