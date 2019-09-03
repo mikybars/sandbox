61d80f4eff4be1bc6eca7721d7afca73ca052107
David Tenreiro Souto <davidtso@ext.inditex.com>
2019-09-03 08:20:11 +0200

    > Programación

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/programacion/repository/ProgramacionRepositoryCustom.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/programacion/repository/ProgramacionRepositoryCustom.java
@@ -1,9 +1,18 @@
 package com.inditex.rrhh.icmclcwb.model.primary.programacion.repository;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 public interface ProgramacionRepositoryCustom {
 
     void reset();
 
     void activa();
+
+    void activa(@Positive @NotNull final Long id);
+
+    void desactiva();
+
+    void desactiva(@Positive @NotNull final Long id);
 
 }
