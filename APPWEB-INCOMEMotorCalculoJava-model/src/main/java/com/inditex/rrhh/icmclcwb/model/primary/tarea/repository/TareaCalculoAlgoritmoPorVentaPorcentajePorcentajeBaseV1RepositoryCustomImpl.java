280bfbf170ab688d26cd655c709094a456fe3df3
Javier Estévez <jestevez@vectoritcgroup.com>
2019-12-17 13:44:45 +0100

    TareaCalculoAlgoritmoPorVentaPorcentajeBaseV1RepositoryCustom

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoAlgoritmoPorVentaPorcentajePorcentajeBaseV1RepositoryCustomImpl.java
@@ -0,0 +1,46 @@
+package com.inditex.rrhh.icmclcwb.model.primary.tarea.repository;
+
+import com.inditex.rrhh.icmclcwb.api.app.calcular.dto.AlgoritmoDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaCalculoPersonaDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.service.TareaCalculoPersonaService;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class TareaCalculoAlgoritmoPorVentaPorcentajePorcentajeBaseV1RepositoryCustomImpl
+    extends AbstractTareaCalculoAlgoritmoBaseRepositoryCustom
+    implements TareaCalculoAlgoritmoPorVentaPorcentajeBaseV1RepositoryCustom {
+
+    //TODO [Comun] Substituir las consultas de las anotaciones @Value
+
+//    @Value("#{calculoPrimaryQuery['TareaCalculoAlgoritmoBaseRepository.calcular.insert']} #{calculoPrimaryQuery['TareaCalculoAlgoritmoPorVentaPorcentajeBaseV1Repository.calcular']} #{calculoPrimaryQuery['TareaCalculoAlgoritmoBaseRepository.calcular.where']}")
+    @Value("#{calculoPrimaryQuery['TareaCalculoAlgoritmoBaseRepository.calcular.insert']} #{calculoPrimaryQuery['TareaCalculoAlgoritmoPorVentaPorcentajeBaseV1Repository.calcular']}")
+    @Getter
+    private String sqlCalcular;
+
+//    @Value("#{calculoPrimaryQuery['TareaCalculoAlgoritmoPorVentaBaseV1Repository.calcular']} #{calculoPrimaryQuery['TareaCalculoAlgoritmoPorVentaPorcentajeBaseV1Repository.calcular.where']}")
+    @Value("#{calculoPrimaryQuery['TareaCalculoAlgoritmoPorVentaPorcentajeBaseV1Repository.calcular']}")
+    @Getter
+    private String sqlCalcularBase;
+
+    @Autowired
+    private TareaCalculoPersonaService tareaCalculoPersonaService;
+
+    @Override
+    protected Map<String, Object> getMapValues(AlgoritmoDto algoritmo, TareaDto tarea, TareaCalculoPersonaDto persona) {
+        //TODO [COMUN] Definir los parametros de la consulta para el cálculo PorVenta
+        return new HashMap<>();
+    }
+
+    @Override
+    public List<TareaCalculoPersonaDto> ids(AlgoritmoDto algoritmo, TareaDto tarea) {
+        return tareaCalculoPersonaService.findByAlgoritmo(tarea, algoritmo);
+    }
+}
