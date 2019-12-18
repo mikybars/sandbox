3cfe3143c560a1bc38ef68032b71f7db68bcc5a5
Javier Estévez <jestevez@vectoritcgroup.com>
2019-12-18 13:49:46 +0100

    TareaCalculoAlgoritmoPorVentaSimplificadoAjusteManualDesplazamientoBaseV1RepositoryCustom TareaCalculoAlgoritmoPorVentaSimplificadoAjusteManualDesplazamientoV1RepositoryCustom TareaCalculoAlgoritmoPorVentaSimplificadoAjusteManualV1RepositoryCustom TareaCalculoAlgoritmoPorVentaSimplificadoManualDesplazamientoBaseV1RepositoryCustom TareaCalculoAlgoritmoPorVentaSimplificadoManualDesplazamientoV1RepositoryCustom

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoAlgoritmoPorVentaSimplificadoManualDesplazamientoV1RepositoryCustom.java
@@ -0,0 +1,5 @@
+package com.inditex.rrhh.icmclcwb.model.primary.tarea.repository;
+
+public interface TareaCalculoAlgoritmoPorVentaSimplificadoManualDesplazamientoV1RepositoryCustom
+    extends TareaCalculoAlgoritmoBaseRepositoryCustom {
+}
