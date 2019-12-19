d83cfea693f5798e0e14e5d5b60bfaa6f4b92d8f
Javier Estévez <jestevez@vectoritcgroup.com>
2019-12-19 09:05:34 +0100

    Enlace de los repository en los cálculos PorVentaSimplificadoPorcentaje

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/calcular/porventasimplificado/v1/PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/calcular/porventasimplificado/v1/PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo.java
@@ -1,13 +1,19 @@
 package com.inditex.rrhh.icmclcwb.model.app.calcular.porventasimplificado.v1;
 
+import com.inditex.aqsw.framework.common.reactor.autoconfiguration.ItxSchedulers;
 import com.inditex.rrhh.icmclcwb.api.app.calcular.dto.AlgoritmoDto;
 import com.inditex.rrhh.icmclcwb.api.app.calcular.properties.dto.RunAlgoritmoPropertiesDto;
 import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.EstadoTareaCalculoPersonaEnum;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.service.TareaCalculoPersonaService;
 import com.inditex.rrhh.icmclcwb.model.app.calcular.RunAlgoritmo;
+import com.inditex.rrhh.icmclcwb.model.app.util.StreamUtils;
+import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 @Component("porVentaSimplificadoPorcentajeDesplazamientoBaseV1")
 public class PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo implements RunAlgoritmo {
@@ -19,15 +25,36 @@ public class PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo impl
     @Qualifier("runAlgoritmoProperties")
     private RunAlgoritmoPropertiesDto runAlgoritmoProperties;
 
+    @Autowired
+    private TareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom
+        tareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom;
+
+    @Autowired
+    private TareaCalculoPersonaService tareaCalculoPersonaService;
+
     @Override
     public void execute(RunTareaDto runTarea, AlgoritmoDto algoritmo) {
-        log.warn("El algoritmo {} no está implementado", algoritmo);
+        Flux.fromIterable(StreamUtils.partition(
+            tareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom.ids(algoritmo, runTarea.getTarea()),
+            runAlgoritmoProperties.getBatchSize())).parallel().runOn(ItxSchedulers.elastic()).map(personas -> {
+            log.info("Inicio :: PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo :: Personas: {}", personas.size());
+            try {
+                tareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom.calcular(algoritmo,
+                    runTarea.getTarea(), personas);
+            } catch (Exception e) {
+                log.error("PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo :: KO :: Personas: {}", personas.size(), e);
+                tareaCalculoPersonaService.updateWithEstadoAndidPersona(personas, runTarea,
+                    EstadoTareaCalculoPersonaEnum.KO.getDto());
+            }
+            log.info("Fin :: PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo :: Personas: {}", personas.size());
+            return Flux.empty();
+        }).sequential().collectList().block();
     }
 
     @Override
     public String getSqlCalcular(AlgoritmoDto algoritmo) {
-        log.warn("El algoritmo {} no está implementado", algoritmo);
-        return "SIN IMPLEMENTAR";
+        return tareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom
+            .getSqlCalcular(algoritmo);
     }
 
 }
