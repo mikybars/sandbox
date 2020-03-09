fef2bef978602a45a2481b5f235100452cc01486
marcelorf <mdelrio@vectoritcgroup.com>
2020-03-09 12:26:06 +0100

    >Correccion en la gestion de errores de politicas

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/ajuste/condiciones/RunAjusteMaximoGarantizadoProcesar.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/ajuste/condiciones/RunAjusteMaximoGarantizadoProcesar.java
@@ -9,6 +9,8 @@ import com.inditex.aqsw.framework.common.reactor.autoconfiguration.ItxSchedulers
 import com.inditex.rrhh.icmclcwb.api.app.ajuste.properties.dto.RunAjustePropertiesDto;
 import com.inditex.rrhh.icmclcwb.api.app.calcular.dto.AlgoritmoAjusteDto;
 import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.EstadoTareaCalculoPersonaEnum;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.service.TareaCalculoPersonaService;
 import com.inditex.rrhh.icmclcwb.model.app.calcular.RunAjuste;
 import com.inditex.rrhh.icmclcwb.model.app.util.StreamUtils;
 import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaCalculoAjusteMaximoGarantizadoRepositoryCustom;
@@ -28,6 +30,9 @@ public class RunAjusteMaximoGarantizadoProcesar implements RunAjuste {
     @Autowired
     private TareaCalculoAjusteMaximoGarantizadoRepositoryCustom tareaCalculoAjusteMaximoGarantizadoRepositoryCustom;
 
+    @Autowired
+    private TareaCalculoPersonaService tareaCalculoPersonaService;
+    
     @Override
     public void execute(RunTareaDto runTarea, AlgoritmoAjusteDto algoritmoAjuste) {
         Flux.fromIterable(StreamUtils.partition(
@@ -38,6 +43,8 @@ public class RunAjusteMaximoGarantizadoProcesar implements RunAjuste {
                         tareaCalculoAjusteMaximoGarantizadoRepositoryCustom.ajustar(algoritmoAjuste, runTarea.getTarea(), personas);
                     } catch (Exception e) {
                         log.error("RunAjusteMaximoGarantizadoProcesar :: KO :: Personas: {}", personas.size(), e);
+                        tareaCalculoPersonaService.updateWithEstadoAndidPersona(personas, runTarea,
+                                EstadoTareaCalculoPersonaEnum.KO.getDto());
                     }
                     log.info("Fin :: RunAjusteMaximoGarantizadoProcesar :: Personas: {}", personas.size());
                     return Flux.empty();
--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/ajuste/condiciones/RunAjusteMinimoGarantizadoProcesar.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/ajuste/condiciones/RunAjusteMinimoGarantizadoProcesar.java
@@ -9,6 +9,8 @@ import com.inditex.aqsw.framework.common.reactor.autoconfiguration.ItxSchedulers
 import com.inditex.rrhh.icmclcwb.api.app.ajuste.properties.dto.RunAjustePropertiesDto;
 import com.inditex.rrhh.icmclcwb.api.app.calcular.dto.AlgoritmoAjusteDto;
 import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.EstadoTareaCalculoPersonaEnum;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.service.TareaCalculoPersonaService;
 import com.inditex.rrhh.icmclcwb.model.app.calcular.RunAjuste;
 import com.inditex.rrhh.icmclcwb.model.app.util.StreamUtils;
 import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaCalculoAjusteMinimoGarantizadoRepositoryCustom;
@@ -28,6 +30,9 @@ public class RunAjusteMinimoGarantizadoProcesar implements RunAjuste {
     @Autowired
     private TareaCalculoAjusteMinimoGarantizadoRepositoryCustom tareaCalculoAjusteMinimoGarantizadoRepositoryCustom;
 
+    @Autowired
+    private TareaCalculoPersonaService tareaCalculoPersonaService;
+    
     @Override
     public void execute(RunTareaDto runTarea, AlgoritmoAjusteDto algoritmoAjuste) {
         Flux.fromIterable(StreamUtils.partition(
@@ -38,6 +43,8 @@ public class RunAjusteMinimoGarantizadoProcesar implements RunAjuste {
                         tareaCalculoAjusteMinimoGarantizadoRepositoryCustom.ajustar(algoritmoAjuste, runTarea.getTarea(), personas);
                     } catch (Exception e) {
                         log.error("RunAjusteMinimoGarantizadoProcesar :: KO :: Personas: {}", personas.size(), e);
+                        tareaCalculoPersonaService.updateWithEstadoAndidPersona(personas, runTarea,
+                                EstadoTareaCalculoPersonaEnum.KO.getDto());
                     }
                     log.info("Fin :: RunAjusteMinimoGarantizadoProcesar :: Personas: {}", personas.size());
                     return Flux.empty();
