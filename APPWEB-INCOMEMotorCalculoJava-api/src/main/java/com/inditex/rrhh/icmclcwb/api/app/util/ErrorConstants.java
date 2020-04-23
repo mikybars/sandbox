a8b9797e07b3c43d909c80d136d060cf952179c2
marcelorf <mdelrio@vectoritcgroup.com>
2020-04-23 10:34:06 +0200

    >Validacion venta operacion sin vendedor

--- APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/util/ErrorConstants.java
+++ APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/util/ErrorConstants.java
@@ -12,5 +12,8 @@ public class ErrorConstants {
     public static final String RUN_ALWORITMO_NOT_IMPLEMENTED_WARNING = "El algoritmo {} no está implementado";
 
     public static final String OPERATION_NOT_VALID = "La operacion {} no es valida";
+    
+    public static final String EMPTY_CCL_ID_PERSON = "El registro {} no tiene vendedor asociado";
+
 
 }
