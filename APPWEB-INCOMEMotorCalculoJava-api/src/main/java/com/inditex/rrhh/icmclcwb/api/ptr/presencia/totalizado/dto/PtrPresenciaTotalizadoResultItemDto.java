330b12a2dae6fc26b151288eec20386fe509fc80
marcelorf <mdelrio@vectoritcgroup.com>
2020-06-19 13:25:59 +0200

    >Correccion ptr y mejora consultas

--- APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/ptr/presencia/totalizado/dto/PtrPresenciaTotalizadoResultItemDto.java
+++ APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/ptr/presencia/totalizado/dto/PtrPresenciaTotalizadoResultItemDto.java
@@ -28,6 +28,9 @@ public class PtrPresenciaTotalizadoResultItemDto {
     private Integer minutos;
 
     private Integer cadena;
+    
+    //TODO: Revisar que este campo sea correcto.
+    private Integer gestionLocal;
 
     private List<PtrSeccionPresenciasGenericType> listaSeccion;
 
