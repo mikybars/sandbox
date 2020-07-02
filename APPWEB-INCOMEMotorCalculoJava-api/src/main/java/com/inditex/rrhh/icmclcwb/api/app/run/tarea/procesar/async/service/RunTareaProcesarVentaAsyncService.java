b586008f43c437fe39a6f9f5ae6640850d4a44be
David Tenreiro Souto <davidtso@ext.inditex.com>
2020-07-02 10:23:32 +0200

    > Merge

--- APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/run/tarea/procesar/async/service/RunTareaProcesarVentaAsyncService.java
+++ APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/run/tarea/procesar/async/service/RunTareaProcesarVentaAsyncService.java
@@ -77,8 +77,4 @@ public interface RunTareaProcesarVentaAsyncService {
 
     CompletableFuture<Void> repartoDevolucionVendedor0(@Valid RunTareaDto tarea);
 
-    CompletableFuture<Void> totalizarVentasVendedor0(RunTareaDto tarea);
-
-    CompletableFuture<Void> repartoDevolucionVendedor0(RunTareaDto tarea);
-
 }
