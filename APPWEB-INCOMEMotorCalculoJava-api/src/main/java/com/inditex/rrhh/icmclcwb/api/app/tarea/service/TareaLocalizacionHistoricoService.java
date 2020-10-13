32d184695021544accde4b8857e3dfdc89997967
marcelorf <mdelrio@vectoritcgroup.com>
2020-10-13 09:33:20 +0200

    >Correccion empresa ficticias y config pro

--- APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/tarea/service/TareaLocalizacionHistoricoService.java
+++ APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/tarea/service/TareaLocalizacionHistoricoService.java
@@ -78,6 +78,6 @@ public interface TareaLocalizacionHistoricoService {
             @NotNull @Positive Long idTarea, @NotNull @Positive Integer idTipoPresupuesto,
             @NotNull LocalDate fechaInicio, @NotNull LocalDate fechaFin);
 
-    void mergeLocalizacionFicticia(@NotNull Long idTarea, @NotNull String cclIdOrigen);
+    void mergeLocalizacionFicticia(@NotNull Long idTarea, @NotNull String cclIdOrigen, @NotBlank String stdIdLegEnt);
 
 }
