16160905f2a4eaa29d5914ec52733581e0585fc1
marcelorf <mdelrio@vectoritcgroup.com>
2021-01-21 16:31:35 +0100

    >Prueba datasources

--- APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/data/DataSourcePtrConfig.java
+++ APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/data/DataSourcePtrConfig.java
@@ -1,6 +1,38 @@
 package com.inditex.rrhh.icmclcwb.config.app.data;
 
-// @Configuration
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import com.inditex.aqsw.framework.data.jdbc.datasources.DataSourceBuilder;
+import com.inditex.aqsw.framework.data.jdbc.datasources.DataSourceType;
+
+@Configuration
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class DataSourcePtrConfig {
+
+    @Bean(name = "ptrDataSource")
+    @ConfigurationProperties(prefix = "amiga.data.jdbc.datasource.ptr")
+    public DataSource ptrDataSource(final DataSourceBuilder dataSourceBuilder) {
+        return dataSourceBuilder.build(DataSourceType.NONXA);
+    }
+
+    @Bean(name = "ptrJdbcTemplate")
+    public JdbcTemplate ptrJdbcTemplate(@Qualifier("ptrDataSource") final DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = "ptrNamedParameterJdbcTemplate")
+    public NamedParameterJdbcTemplate ptrNamedParameterJdbcTemplate(
+            @Qualifier("ptrJdbcTemplate") final JdbcTemplate jdbcTemplate) {
+        return new NamedParameterJdbcTemplate(jdbcTemplate);
+    }
 
 }
