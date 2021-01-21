78825309e4fc1b2120d0682af97d6663bb8926b5
marcelorf <mdelrio@vectoritcgroup.com>
2021-01-21 16:48:37 +0100

    >Prueba datasources

--- APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/data/DataSourcePrimaryConfig.java
+++ APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/app/data/DataSourcePrimaryConfig.java
@@ -1,45 +1,60 @@
 package com.inditex.rrhh.icmclcwb.config.app.data;
 
-// @Configuration
-// @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
-// @AmigaEnableJpaRepositories(entityManagerFactoryRef = "primaryEntityManagerFactory", basePackages
-// = {
-// "com.inditex.rrhh.icmclcwb.model.primary" })
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import com.inditex.aqsw.framework.data.jdbc.datasources.DataSourceBuilder;
+import com.inditex.aqsw.framework.data.jdbc.datasources.DataSourceType;
+import com.inditex.aqsw.framework.data.jpa.annotations.AmigaEnableJpaRepositories;
+
+@Configuration
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@AmigaEnableJpaRepositories(entityManagerFactoryRef = "primaryEntityManagerFactory", basePackages = {
+        "com.inditex.rrhh.icmclcwb.model.primary" })
 public class DataSourcePrimaryConfig {
 
-    // @Bean(name = "primaryDataSource")
-    // @Primary
-    // @ConfigurationProperties(prefix = "amiga.data.jdbc.datasource.primary")
-    // public DataSource primaryDataSource(final DataSourceBuilder dataSourceBuilder) {
-    // return dataSourceBuilder.build(DataSourceType.XA);
-    // }
-    //
-    // @Bean(name = "primaryEntityManagerFactory")
-    // @Primary
-    // public LocalContainerEntityManagerFactoryBean primaryEntityManagerFactory(
-    // final EntityManagerFactoryBuilder entityManagerFactoryBuilder,
-    // final JpaProperties jpaProperties,
-    // @Qualifier("primaryDataSource") final DataSource dataSource) {
-    // return entityManagerFactoryBuilder.dataSource(dataSource)
-    // .persistenceUnit("primaryPersistenceUnit")
-    // .packages("com.inditex.rrhh.icmclcwb.model.primary")
-    // .properties(jpaProperties.getProperties())
-    // .jta(true)
-    // .build();
-    // }
-    //
-    // @Bean(name = "primaryJdbcTemplate")
-    // @Primary
-    // public JdbcTemplate primaryJdbcTemplate(@Qualifier("primaryDataSource") final DataSource
-    // dataSource) {
-    // return new JdbcTemplate(dataSource);
-    // }
-    //
-    // @Bean(name = "primaryNamedParameterJdbcTemplate")
-    // @Primary
-    // public NamedParameterJdbcTemplate primaryNamedParameterJdbcTemplate(
-    // @Qualifier("primaryJdbcTemplate") final JdbcTemplate jdbcTemplate) {
-    // return new NamedParameterJdbcTemplate(jdbcTemplate);
-    // }
+    @Bean(name = "primaryDataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "amiga.data.jdbc.datasource.primary")
+    public DataSource primaryDataSource(final DataSourceBuilder dataSourceBuilder) {
+        return dataSourceBuilder.build(DataSourceType.XA);
+    }
+
+    @Bean(name = "primaryEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean primaryEntityManagerFactory(
+            final EntityManagerFactoryBuilder entityManagerFactoryBuilder,
+            final JpaProperties jpaProperties,
+            @Qualifier("primaryDataSource") final DataSource dataSource) {
+        return entityManagerFactoryBuilder.dataSource(dataSource)
+            .persistenceUnit("primaryPersistenceUnit")
+            .packages("com.inditex.rrhh.icmclcwb.model.primary")
+            .properties(jpaProperties.getProperties())
+            .jta(true)
+            .build();
+    }
+
+    @Bean(name = "primaryJdbcTemplate")
+    public JdbcTemplate primaryJdbcTemplate(@Qualifier("primaryDataSource") final DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = "primaryNamedParameterJdbcTemplate")
+    @Primary
+    public NamedParameterJdbcTemplate primaryNamedParameterJdbcTemplate(
+            @Qualifier("primaryJdbcTemplate") final JdbcTemplate jdbcTemplate) {
+        return new NamedParameterJdbcTemplate(jdbcTemplate);
+    }
 
 }
