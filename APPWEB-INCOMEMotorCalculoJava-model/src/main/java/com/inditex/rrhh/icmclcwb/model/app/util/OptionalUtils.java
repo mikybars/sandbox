4f4c56939458099469c0b077f7542b99a4cf43be
Javier Estévez <jestevezv@vectoritcgroup.com>
2021-02-04 14:50:10 +0100

    Resolución de errores detectados por Sonar:  - Uso de la nueva clase OptionalUtils para obtener los datos de un optional  - Interrupción del hilo si se produce InterruptedException

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/util/OptionalUtils.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021.  Inditex
+ */
+
+package com.inditex.rrhh.icmclcwb.model.app.util;
+
+import java.util.Optional;
+
+public class OptionalUtils {
+
+    private OptionalUtils() {
+    }
+
+    public static <T> T get(final Optional<T> optional) {
+        return get(optional, null);
+    }
+
+    public static <T> T get(final Optional<T> optional, final T defaultValue) {
+        return optional.isPresent() ? optional.get() : defaultValue;
+    }
+}
