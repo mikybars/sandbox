49e572d6247fa127908202cf86d7dfbc8cfe432b
marcelorf <mdelrio@vectoritcgroup.com>
2021-02-25 17:36:28 +0100

    >Correccion test

--- APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/RunTareaValidarMotivosDesplazamientoServiceImplTest.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/RunTareaValidarMotivosDesplazamientoServiceImplTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import com.inditex.rrhh.icmclcwb.api.app.dto.ValidacionDto;
 import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
 import com.inditex.rrhh.icmclcwb.api.app.run.tarea.validar.service.RunTareaAmbitoValidarMotivosDesplazamientoService;
+import com.inditex.rrhh.icmclcwb.api.app.service.MailService;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaAmbitoDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaFaseAccionDto;
@@ -38,6 +39,9 @@ public class RunTareaValidarMotivosDesplazamientoServiceImplTest {
 
     @InjectMocks
     private RunTareaValidarMotivosDesplazamientoServiceImpl runTareaValidarMotivosDesplazamientoService;
+
+    @Mock
+    private MailService mailService;
 
     @Test
     public void execute() {
