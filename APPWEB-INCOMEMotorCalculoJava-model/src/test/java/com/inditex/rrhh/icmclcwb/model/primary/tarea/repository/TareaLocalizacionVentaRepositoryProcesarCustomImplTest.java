9e32f63edffe179d4a0507b89c5dc00504d7adf6
Javier Estévez <jestevezv@vectoritcgroup.com>
2021-04-20 08:31:25 +0200

    Reparto de la venta online entrega tienda por venta entre los días del período en los que está abierta la tienda

--- APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaLocalizacionVentaRepositoryProcesarCustomImplTest.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaLocalizacionVentaRepositoryProcesarCustomImplTest.java
@@ -1,5 +1,7 @@
 package com.inditex.rrhh.icmclcwb.model.primary.tarea.repository;
 
+import java.time.LocalDate;
+
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
@@ -7,6 +9,7 @@ import com.inditex.rrhh.icmclcwb.api.app.TipoVentaConceptoEnum;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.TipoDatoEnum;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
 import com.inditex.rrhh.icmclcwb.api.app.util.AppConstants;
+import com.inditex.rrhh.icmclcwb.model.app.util.TimeUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,6 +21,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static com.inditex.rrhh.icmclcwb.api.app.util.SqlPrimaryConstants.SQL_PARAM_ACTIVO;
+import static com.inditex.rrhh.icmclcwb.api.app.util.SqlPrimaryConstants.SQL_PARAM_FECHA_FIN;
 import static com.inditex.rrhh.icmclcwb.api.app.util.SqlPrimaryConstants.SQL_PARAM_ID_CONCEPTO;
 import static com.inditex.rrhh.icmclcwb.api.app.util.SqlPrimaryConstants.SQL_PARAM_ID_SECCION;
 import static com.inditex.rrhh.icmclcwb.api.app.util.SqlPrimaryConstants.SQL_PARAM_ID_TAREA;
@@ -60,9 +64,9 @@ public class TareaLocalizacionVentaRepositoryProcesarCustomImplTest {
 
     @Before
     public void setup() throws IllegalAccessException {
-        FieldUtils.writeField(tareaLocalizacionVentaRepositoryProcesarCustom,
+        FieldUtils.writeField(this.tareaLocalizacionVentaRepositoryProcesarCustom,
                 "sqlProcesarEntregaDomicilioAgrupaciones", SQL_PROCESAR_REPARTO_ENTREGA_DOMICILIO_AGRUPACIONES, true);
-        FieldUtils.writeField(tareaLocalizacionVentaRepositoryProcesarCustom,
+        FieldUtils.writeField(this.tareaLocalizacionVentaRepositoryProcesarCustom,
                 "sqlProcesarEntregaDomicilioPresenciaAgrupaciones",
                 SQL_PROCESAR_REPARTO_ETNREGA_DOMICILIO_POR_PRESENCIA_AGRUPACIONES, true);
     }
@@ -70,18 +74,18 @@ public class TareaLocalizacionVentaRepositoryProcesarCustomImplTest {
     @Test
     public void procesarRepartoEntregaDomicilioAgrupacionesTest() {
 
-        TareaDto tarea = mock(TareaDto.class);
+        final TareaDto tarea = mock(TareaDto.class);
         when(tarea.getId()).thenReturn(7474L);
+        when(tarea.getFechaFinPeriodo()).thenReturn(LocalDate.of(2020, 8, 31));
 
-        tareaLocalizacionVentaRepositoryProcesarCustom.procesarRepartoEntregaDomicilioAgrupaciones(tarea);
-        verify(namedParameterJdbcTemplate, times(1)).update(sqlCaptor.capture(), paramsCaptor.capture());
-        assertEquals(SQL_PROCESAR_REPARTO_ENTREGA_DOMICILIO_AGRUPACIONES, sqlCaptor.getValue());
-        MapSqlParameterSource params = paramsCaptor.getValue();
+        this.tareaLocalizacionVentaRepositoryProcesarCustom.procesarRepartoEntregaDomicilioAgrupaciones(tarea);
+        verify(this.namedParameterJdbcTemplate, times(1)).update(this.sqlCaptor.capture(), this.paramsCaptor.capture());
+        assertEquals(SQL_PROCESAR_REPARTO_ENTREGA_DOMICILIO_AGRUPACIONES, this.sqlCaptor.getValue());
+        final MapSqlParameterSource params = this.paramsCaptor.getValue();
         // Parámetros de la consulta: nuevoIdTipoDato, nuevoActivo, idTipoImporteVentaFisicaAgrupacion,
         // idTipoImporteVentaEntregaDomicilioAgrupacion, idTarea, idTipoImporteVentaFisicaLocalizacion,
-        // idSeccion,
-        // idConcepto, porcentajeInclusion
-        assertEquals(9, params.getValues().size());
+        // idSeccion, fechaFin, idConcepto, porcentajeInclusion
+        assertEquals(10, params.getValues().size());
         // nuevoIdTipoDato,
         assertTrue(params.hasValue(SQL_PARAM_NUEVO_ID_TIPO_DATO));
         assertEquals(TipoDatoEnum.VENTA_ONLINE_ENTREGADOMICILIO_LOCALIZACION.getId(),
@@ -113,18 +117,22 @@ public class TareaLocalizacionVentaRepositoryProcesarCustomImplTest {
         // porcentajeInclusion
         assertTrue(params.hasValue(SQL_PARAM_PORCENTAJE_INCLUSION));
         assertEquals(SQL_VALUE_PORCENTAJE_CERO, params.getValue(SQL_PARAM_PORCENTAJE_INCLUSION));
+        // fechaFin
+        assertTrue(params.hasValue(SQL_PARAM_FECHA_FIN));
+        assertEquals(TimeUtils.toDate(tarea.getFechaFinPeriodo()), params.getValue(SQL_PARAM_FECHA_FIN));
     }
 
     @Test
     public void procesarRepartoEntregaDomicilioPorPresenciaAgrupacionesTest() {
 
-        TareaDto tarea = mock(TareaDto.class);
+        final TareaDto tarea = mock(TareaDto.class);
         when(tarea.getId()).thenReturn(7474L);
 
-        tareaLocalizacionVentaRepositoryProcesarCustom.procesarRepartoEntregaDomicilioPorPresenciaAgrupaciones(tarea);
-        verify(namedParameterJdbcTemplate, times(1)).update(sqlCaptor.capture(), paramsCaptor.capture());
-        assertEquals(SQL_PROCESAR_REPARTO_ETNREGA_DOMICILIO_POR_PRESENCIA_AGRUPACIONES, sqlCaptor.getValue());
-        MapSqlParameterSource params = paramsCaptor.getValue();
+        this.tareaLocalizacionVentaRepositoryProcesarCustom
+            .procesarRepartoEntregaDomicilioPorPresenciaAgrupaciones(tarea);
+        verify(this.namedParameterJdbcTemplate, times(1)).update(this.sqlCaptor.capture(), this.paramsCaptor.capture());
+        assertEquals(SQL_PROCESAR_REPARTO_ETNREGA_DOMICILIO_POR_PRESENCIA_AGRUPACIONES, this.sqlCaptor.getValue());
+        final MapSqlParameterSource params = this.paramsCaptor.getValue();
         // Parámetros de la consulta: nuevoIdTipoDato, nuevoActivo, idTipoPresenciaLocalizacion, activo,
         // idTipoPresenciaAgrupacion, idTipoImporteVentaEntregaDomicilioAgrupacion, idTarea,
         // idTipoImporteVentaFisicaLocalizacion, idSeccion,
