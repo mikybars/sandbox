d382486ad3d429e73b908da125b42cf69d5a82b1
marcelorf <mdelrio@vectoritcgroup.com>
2021-06-15 12:56:41 +0200

    >Correccion ajuste manual y borrado ajuste manual/resalta manual con desplazamientos

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoAlgoritmoResaltaManualDesplazamientoBaseV1RepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package com.inditex.rrhh.icmclcwb.model.primary.tarea.repository;
-
-public interface TareaCalculoAlgoritmoResaltaManualDesplazamientoBaseV1RepositoryCustom
-        extends TareaCalculoAlgoritmoBaseRepositoryCustom {
-
-}
