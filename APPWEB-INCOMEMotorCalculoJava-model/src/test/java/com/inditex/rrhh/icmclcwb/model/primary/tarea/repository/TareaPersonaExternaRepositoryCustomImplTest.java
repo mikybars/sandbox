76ccd411f78bae31d57025ec218788fc6e092432
Javier Estévez <jestevezv@vectoritcgroup.com>
2021-06-21 13:21:52 +0200

    Tests sobre el guardado de empleados externos

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaPersonaExternaRepositoryCustomImplTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2021. Inditex
+ */
+
+package com.inditex.rrhh.icmclcwb.model.primary.tarea.repository;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import com.inditex.rrhh.icmclcwb.model.primary.tarea.entity.TareaPersonaExterna;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TareaPersonaExternaRepositoryCustomImplTest {
+
+    private final static String SQL_SAVE = "SQL SAVE";
+
+    @Mock
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @InjectMocks
+    private TareaPersonaExternaRepositoryCustomImpl tareaPersonaExternaRepositoryCustom;
+
+    @Captor
+    private ArgumentCaptor<String> sqlCaptor;
+
+    @Before
+    public void setup() throws IllegalAccessException {
+        FieldUtils.writeField(this.tareaPersonaExternaRepositoryCustom, "sqlSave", SQL_SAVE, true);
+        FieldUtils.writeField(this.tareaPersonaExternaRepositoryCustom, "batchSize", 100, true);
+    }
+
+    @Test
+    public void saveTest() {
+        final List<TareaPersonaExterna> items = Arrays.asList(mock(TareaPersonaExterna.class));
+        this.tareaPersonaExternaRepositoryCustom.save(items);
+        verify(this.namedParameterJdbcTemplate).batchUpdate(this.sqlCaptor.capture(),
+                any(SqlParameterSource[].class));
+        assertEquals(SQL_SAVE, this.sqlCaptor.getValue());
+    }
+
+}
