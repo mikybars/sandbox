0d773181d639f6cdccc997c2cee8d53e7a9f16ee
Javier Estévez <jestevezv@vectoritcgroup.com>
2021-06-21 10:51:43 +0200

    Obtención de empleados externos en Bosnia

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/RunTareaAmbitoValidarExternosBsoniaServiceImplTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2021. Inditex
+ */
+package com.inditex.rrhh.icmclcwb.model.app.run.tarea.validar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import com.inditex.rrhh.icmclcwb.api.app.ComisClaseEmpleadoEnum;
+import com.inditex.rrhh.icmclcwb.api.app.async.service.ComisAsyncService;
+import com.inditex.rrhh.icmclcwb.api.app.dto.IdPersonaLocalExternaDto;
+import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaAmbitoDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaFaseAccionDto;
+import com.inditex.rrhh.icmclcwb.model.app.run.tarea.validar.mapper.ValidacionMapper;
+import com.inditex.rrhh.icmclcwb.model.app.tarea.mapper.TareaPersonaExternaMapper;
+import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaPersonaExternaRepositoryCustom;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RunTareaAmbitoValidarExternosBsoniaServiceImplTest {
+
+    @Mock
+    private ComisAsyncService comisAsyncService;
+
+    @Mock
+    private TareaPersonaExternaRepositoryCustom tareaPersonaExternaRepositoryCustom;
+
+    @Mock
+    private TareaPersonaExternaMapper tareaPersonaExternaMapper;
+
+    @Mock
+    private ValidacionMapper validacionMapper;
+
+    @InjectMocks
+    private RunTareaAmbitoValidarExternosBosniaServiceImpl runTareaAmbitoValidarExternosBosniaService;
+
+    @Test
+    public void executeTest() {
+
+        final RunTareaDto runTareaDto = new RunTareaDto();
+        final TareaDto tareaDto = new TareaDto();
+        tareaDto.setId(1L);
+        runTareaDto.setTarea(tareaDto);
+        final TareaAmbitoDto tareaAmbitoDto = new TareaAmbitoDto();
+        final TareaFaseAccionDto tareaFaseAccionDto = new TareaFaseAccionDto();
+
+        final List<IdPersonaLocalExternaDto> lista = new ArrayList<>();
+        final CompletableFuture<List<IdPersonaLocalExternaDto>> cf = new CompletableFuture<>();
+        cf.complete(lista);
+
+        when(this.comisAsyncService.findExternosByClase(any(RunTareaDto.class), any(TareaAmbitoDto.class), any(
+                ComisClaseEmpleadoEnum.class))).thenReturn(cf);
+
+        this.runTareaAmbitoValidarExternosBosniaService.execute(runTareaDto, tareaAmbitoDto, tareaFaseAccionDto);
+
+        verify(this.comisAsyncService, timeout(1000).times(1)).findExternosByClase(runTareaDto, tareaAmbitoDto,
+                ComisClaseEmpleadoEnum.EMPLEADO_EXTERNO_BOSNIA);
+        verify(this.tareaPersonaExternaMapper, timeout(1000).times(1))
+            .idPersonaLocalExternaToTareaPersonaExterna(any(List.class), eq(tareaDto));
+        verify(this.tareaPersonaExternaRepositoryCustom, timeout(1000).times(1)).save(any(List.class));
+        verify(this.validacionMapper, timeout(1000).times(1)).booleanToValidacionDto(tareaAmbitoDto, tareaFaseAccionDto,
+                true);
+    }
+
+}
