3d0a690aef7f366e21e9c2b7a10102b70f9d7ed6
Javier Estévez <jestevezv@vectoritcgroup.com>
2021-08-13 15:25:02 +0200

    Correcciones tests

--- APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/calcular/globaltiendaseccion/v1/GlobalTiendaSeccionPorcentajeV1RunAlgoritmoTest.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/calcular/globaltiendaseccion/v1/GlobalTiendaSeccionPorcentajeV1RunAlgoritmoTest.java
@@ -2,6 +2,7 @@ package com.inditex.rrhh.icmclcwb.model.app.calcular.globaltiendaseccion.v1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import com.inditex.rrhh.icmclcwb.api.app.calcular.dto.AlgoritmoDto;
 import com.inditex.rrhh.icmclcwb.api.app.calcular.properties.dto.RunAlgoritmoPropertiesDto;
@@ -10,6 +11,8 @@ import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.EstadoTareaCalculoPersonaEnum;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.service.TareaCalculoPersonaService;
+import com.inditex.rrhh.icmclcwb.api.app.util.AsyncConstants;
+import com.inditex.rrhh.icmclcwb.model.app.calcular.RunAlgoritmoTest;
 import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaCalculoAlgoritmoGlobalTiendaSeccionPorcentajeV1RepositoryCustom;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,13 +24,14 @@ import org.slf4j.Logger;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class GlobalTiendaSeccionPorcentajeV1RunAlgoritmoTest {
+public class GlobalTiendaSeccionPorcentajeV1RunAlgoritmoTest implements RunAlgoritmoTest {
 
     private final static String SQL_CALCULAR = "SELECT * FROM TABLE WHERE 1";
 
@@ -59,7 +63,7 @@ public class GlobalTiendaSeccionPorcentajeV1RunAlgoritmoTest {
     @Test
     public void calcularTest() {
 
-        when(this.runAlgoritmoPropertiesDto.getCalculo().getBatchSize()).thenReturn(10);
+        when(this.runAlgoritmoPropertiesDto.getCalculo()).thenReturn(this.createRunAlgoritmoCalculoPropertiesDto(10));
 
         final List<IdPersonaLocalDto> personas = new ArrayList<>();
         final IdPersonaLocalDto p1 = new IdPersonaLocalDto();
@@ -70,17 +74,25 @@ public class GlobalTiendaSeccionPorcentajeV1RunAlgoritmoTest {
         personas.add(p3);
         when(this.tareaCalculoAlgoritmoGlobalTiendaSeccionPorcentajeV1RepositoryCustom
             .ids(any(AlgoritmoDto.class), any(TareaDto.class))).thenReturn(personas);
+        when(this.tareaCalculoAlgoritmoGlobalTiendaSeccionPorcentajeV1RepositoryCustom.calcular(
+                any(AlgoritmoDto.class),
+                any(TareaDto.class), anyList())).thenReturn(
+                        CompletableFuture.completedFuture(AsyncConstants.NIL));
 
-        final RunTareaDto runTarea = new RunTareaDto();
-        final TareaDto tarea = new TareaDto();
-        runTarea.setTarea(tarea);
+        final long idTarea = 123L;
+        final long idTrabajo = 5675L;
+        final RunTareaDto runTarea = this.createRunTareaDto(idTarea, idTrabajo);
         final AlgoritmoDto algoritmo = new AlgoritmoDto();
         this.globalTiendaSeccionPorcentajeV1RunAlgoritmo.execute(runTarea, algoritmo);
 
-        verify(this.log, times(1)).info("Inicio :: GlobalTiendaSeccionPorcentajeV1RunAlgoritmo :: Personas: {}", 3);
+        verify(this.log, times(1))
+            .info("Trabajo[{}]Tarea[{}] :: Inicio :: GlobalTiendaSeccionPorcentajeV1RunAlgoritmo :: Personas: {}",
+                    idTrabajo, idTarea, 3);
         verify(this.tareaCalculoAlgoritmoGlobalTiendaSeccionPorcentajeV1RepositoryCustom, times(1))
-            .calcular(algoritmo, tarea, personas);
-        verify(this.log, times(1)).info("Fin :: GlobalTiendaSeccionPorcentajeV1RunAlgoritmo :: Personas: {}", 3);
+            .calcular(algoritmo, runTarea.getTarea(), personas);
+        verify(this.log, times(1))
+            .info("Trabajo[{}]Tarea[{}] :: Fin :: GlobalTiendaSeccionPorcentajeV1RunAlgoritmo :: Personas: {}",
+                    idTrabajo, idTarea, 3);
     }
 
     @Test
@@ -94,20 +106,21 @@ public class GlobalTiendaSeccionPorcentajeV1RunAlgoritmoTest {
         when(this.tareaCalculoAlgoritmoGlobalTiendaSeccionPorcentajeV1RepositoryCustom
             .ids(any(AlgoritmoDto.class), any(TareaDto.class))).thenReturn(personas);
 
-        when(this.runAlgoritmoPropertiesDto.getCalculo().getBatchSize()).thenReturn(2);
+        when(this.runAlgoritmoPropertiesDto.getCalculo()).thenReturn(this.createRunAlgoritmoCalculoPropertiesDto(2));
         final RuntimeException exception = new RuntimeException("EEEE");
         doThrow(exception).when(this.tareaCalculoAlgoritmoGlobalTiendaSeccionPorcentajeV1RepositoryCustom)
             .calcular(any(AlgoritmoDto.class), any(TareaDto.class),
                     ArgumentMatchers.<List<IdPersonaLocalDto>>any());
 
-        final RunTareaDto runTarea = new RunTareaDto();
-        final TareaDto tarea = new TareaDto();
-        runTarea.setTarea(tarea);
+        final long idTarea = 123L;
+        final long idTrabajo = 5675L;
+        final RunTareaDto runTarea = this.createRunTareaDto(idTarea, idTrabajo);
         final AlgoritmoDto algoritmo = new AlgoritmoDto();
         this.globalTiendaSeccionPorcentajeV1RunAlgoritmo.execute(runTarea, algoritmo);
 
         verify(this.log, times(1))
-            .error("GlobalTiendaSeccionPorcentajeV1RunAlgoritmo :: KO :: Personas: {}", 2, exception);
+            .error("Trabajo[{}]Tarea[{}] :: GlobalTiendaSeccionPorcentajeV1RunAlgoritmo :: KO :: Personas: {}",
+                    idTrabajo, idTarea, 2, exception);
         verify(this.tareaCalculoPersonaService, times(1)).updateWithEstadoAndidPersona(personas, runTarea,
                 EstadoTareaCalculoPersonaEnum.KO.getDto());
     }
--- APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/calcular/porventasimplificado/v1/PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmoTest.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/calcular/porventasimplificado/v1/PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmoTest.java
@@ -2,6 +2,7 @@ package com.inditex.rrhh.icmclcwb.model.app.calcular.porventasimplificado.v1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import com.inditex.rrhh.icmclcwb.api.app.calcular.dto.AlgoritmoDto;
 import com.inditex.rrhh.icmclcwb.api.app.calcular.properties.dto.RunAlgoritmoPropertiesDto;
@@ -10,6 +11,8 @@ import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.EstadoTareaCalculoPersonaEnum;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.service.TareaCalculoPersonaService;
+import com.inditex.rrhh.icmclcwb.api.app.util.AsyncConstants;
+import com.inditex.rrhh.icmclcwb.model.app.calcular.RunAlgoritmoTest;
 import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,13 +24,14 @@ import org.slf4j.Logger;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmoTest {
+public class PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmoTest implements RunAlgoritmoTest {
 
     private final static String SQL_CALCULAR = "SELECT * FROM TABLE WHERE 1";
 
@@ -60,7 +64,7 @@ public class PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmoTest 
     @Test
     public void calcularTest() {
 
-        when(this.runAlgoritmoPropertiesDto.getCalculo().getBatchSize()).thenReturn(10);
+        when(this.runAlgoritmoPropertiesDto.getCalculo()).thenReturn(this.createRunAlgoritmoCalculoPropertiesDto(10));
 
         final List<IdPersonaLocalDto> personas = new ArrayList<>();
         final IdPersonaLocalDto p1 = new IdPersonaLocalDto();
@@ -71,19 +75,25 @@ public class PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmoTest 
         personas.add(p3);
         when(this.tareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom
             .ids(any(AlgoritmoDto.class), any(TareaDto.class))).thenReturn(personas);
+        when(this.tareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom.calcular(
+                any(AlgoritmoDto.class),
+                any(TareaDto.class), anyList())).thenReturn(
+                        CompletableFuture.completedFuture(AsyncConstants.NIL));
 
-        final RunTareaDto runTarea = new RunTareaDto();
-        final TareaDto tarea = new TareaDto();
-        runTarea.setTarea(tarea);
+        final long idTarea = 123L;
+        final long idTrabajo = 5675L;
+        final RunTareaDto runTarea = this.createRunTareaDto(idTarea, idTrabajo);
         final AlgoritmoDto algoritmo = new AlgoritmoDto();
         this.porVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo.execute(runTarea, algoritmo);
 
         verify(this.log, times(1))
-            .info("Inicio :: PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo :: Personas: {}", 3);
+            .info("Trabajo[{}]Tarea[{}] :: Inicio :: PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo :: Personas: {}",
+                    idTrabajo, idTarea, 3);
         verify(this.tareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom, times(1))
-            .calcular(algoritmo, tarea, personas);
+            .calcular(algoritmo, runTarea.getTarea(), personas);
         verify(this.log, times(1))
-            .info("Fin :: PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo :: Personas: {}", 3);
+            .info("Trabajo[{}]Tarea[{}] :: Fin :: PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo :: Personas: {}",
+                    idTrabajo, idTarea, 3);
     }
 
     @Test
@@ -97,22 +107,22 @@ public class PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmoTest 
         when(this.tareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom
             .ids(any(AlgoritmoDto.class), any(TareaDto.class))).thenReturn(personas);
 
-        when(this.runAlgoritmoPropertiesDto.getCalculo().getBatchSize()).thenReturn(2);
+        when(this.runAlgoritmoPropertiesDto.getCalculo()).thenReturn(this.createRunAlgoritmoCalculoPropertiesDto(2));
         final RuntimeException exception = new RuntimeException("EEEE");
         doThrow(exception)
             .when(this.tareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom)
             .calcular(any(AlgoritmoDto.class), any(TareaDto.class),
                     ArgumentMatchers.<List<IdPersonaLocalDto>>any());
 
-        final RunTareaDto runTarea = new RunTareaDto();
-        final TareaDto tarea = new TareaDto();
-        runTarea.setTarea(tarea);
+        final long idTarea = 123L;
+        final long idTrabajo = 5675L;
+        final RunTareaDto runTarea = this.createRunTareaDto(idTarea, idTrabajo);
         final AlgoritmoDto algoritmo = new AlgoritmoDto();
         this.porVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo.execute(runTarea, algoritmo);
 
         verify(this.log, times(1))
-            .error("PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo :: KO :: Personas: {}", 2,
-                    exception);
+            .error("Trabajo[{}]Tarea[{}] :: PorVentaSimplificadoPorcentajeDesplazamientoBaseV1RunAlgoritmo :: KO :: Personas: {}",
+                    idTrabajo, idTarea, 2, exception);
         verify(this.tareaCalculoPersonaService, times(1)).updateWithEstadoAndidPersona(personas, runTarea,
                 EstadoTareaCalculoPersonaEnum.KO.getDto());
     }
