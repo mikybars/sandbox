977c70e2f6bd7d7437ff5f3fca1307a8282b61b3
Javier Estévez <jestevezv@vectoritcgroup.com>
2021-08-31 14:46:48 +0200

    Multi-prima:  - Scripts de creación de los nuevos tipos de comisión  - TipoComisionEnum

--- APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/calcular/TipoComisionEnum.java
+++ APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/calcular/TipoComisionEnum.java
@@ -11,7 +11,8 @@ public enum TipoComisionEnum {
             "005"), COMISION_POR_TOPES("006"), MANUAL("007"), COMISION_DIARIA("008"), PRIMA("009"), AJUSTE_MANUAL(
                     "010"), CHALLENGE_PRINCIPAL(
                             "011"), CHALLENGE_SECUNDARIO(
-                                    "012"), PRECIO_HORA("013"), RESALTA_MANUAL("014"), COMISION_PROVINCIA("015");
+                                    "012"), PRECIO_HORA("013"), RESALTA_MANUAL("014"), COMISION_PROVINCIA(
+                                            "015"), PRIMA_FUNCION("016"), PRIMA_PERSONAL("017");
 
     private final String id;
 
