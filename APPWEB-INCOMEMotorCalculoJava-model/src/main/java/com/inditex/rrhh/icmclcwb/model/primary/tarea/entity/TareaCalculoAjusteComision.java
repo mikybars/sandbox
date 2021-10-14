e181e20f16e5c4ce97e14f620bca5486b83abd7f
Javier Estévez <jestevezv@vectoritcgroup.com>
2021-10-14 10:42:27 +0200

    Entidades para la tabla TAREA_CALCULO_AJUSTE_COMISION

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/entity/TareaCalculoAjusteComision.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2021. Inditex
+ */
+
+package com.inditex.rrhh.icmclcwb.model.primary.tarea.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.inditex.rrhh.icmclcwb.model.primary.calcular.entity.TipoComision;
+import lombok.Data;
+
+@Entity
+@Table(name = "TAREA_CALCULO_AJUSTE_COMISION")
+@Data
+public class TareaCalculoAjusteComision {
+
+    @EmbeddedId
+    private TareaCalculoAjusteComisionPk pk;
+
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "ID_TAREA", nullable = false)
+    private Tarea tarea;
+
+    @NotBlank
+    @Column(name = "CCL_ID_PERSON", nullable = false, length = 48)
+    private String cclIdPerson;
+
+    @NotBlank
+    @Column(name = "STD_OR_HR_PERIOD", nullable = false, length = 48)
+    private String stdOrHrPeriod;
+
+    @NotNull
+    @Column(name = "IMPORTE", nullable = false, precision = 23, scale = 8)
+    private BigDecimal importe;
+
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "ID_TIPO_COMISION", nullable = false)
+    private TipoComision tipoComision;
+
+}
