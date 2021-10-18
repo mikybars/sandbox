c9f3b14d611e49e3bc6ee0e53b2b39b2ccfe3321
albertoggu <albertoggu@ext.inditex.com>
2021-10-18 09:40:52 +0200

    delete unused import

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/trabajo/service/TrabajoServiceImpl.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/trabajo/service/TrabajoServiceImpl.java
@@ -2,7 +2,6 @@ package com.inditex.rrhh.icmclcwb.model.app.trabajo.service;
 
 import java.time.ZoneOffset;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
