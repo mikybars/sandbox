f488f5d7226f4766e3c782172ee0b2d6bc7977f3
albertoggu <albertoggu@ext.inditex.com>
2021-11-09 09:34:28 +0100

    Tests HealthUtil & TypeHealthEnum

---
+++ APPWEB-INCOMEMotorCalculoJava-api/src/test/java/com/inditex/rrhh/icmclcwb/api/utils/TypeHealthEnumTest.java
@@ -0,0 +1,23 @@
+package com.inditex.rrhh.icmclcwb.api.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class TypeHealthEnumTest {
+
+    @Test
+    public void getIdTest () {
+        assertEquals(TypeHealthEnum.AMIGA_HEALTH_STATUS_UP.getId(), 1);
+        assertEquals(TypeHealthEnum.VALIDATE_RESPONSE_STATUS_CODE_OK.getId(), 2);
+    }
+
+    @Test
+    public void gtByIdTest () {
+        assertEquals(TypeHealthEnum.getById(1), TypeHealthEnum.AMIGA_HEALTH_STATUS_UP);
+        assertEquals(TypeHealthEnum.getById(2), TypeHealthEnum.VALIDATE_RESPONSE_STATUS_CODE_OK);
+        assertNull(TypeHealthEnum.getById(3));
+    }
+
+}
---
+++ APPWEB-INCOMEMotorCalculoJava-ws/src/test/java/com/inditex/rrhh/icmclcwb/ws/health/utils/HealthUtilTest.java
@@ -0,0 +1,65 @@
+package com.inditex.rrhh.icmclcwb.ws.health.utils;
+
+import com.inditex.rrhh.icmclcwb.api.utils.TypeHealthEnum;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.actuate.health.Health;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HealthUtilTest {
+
+    @InjectMocks
+    HealthUtil healthUtil;
+
+    @Test
+    public void callEndpointTest1 () {
+        Map<String, String> dataProject = new HashMap<>();
+        dataProject.put("projectService", "ICMCLCWB");
+
+        Health health = this.healthUtil.callEndpoint(false, dataProject, null,
+            null, null, null);
+
+        assertNotNull(health);
+        assertEquals(health, Health.unknown().withDetail("project", "ICMCLCWB").build());
+    }
+
+    @Test
+    public void callEndpointTest2 () {
+        Map<String, String> dataProject = new HashMap<>();
+        dataProject.put("projectService", "ICMCLCWB");
+        dataProject.put("usernameService", "user");
+        dataProject.put("passwordService", "pass");
+        dataProject.put("authMethodService", "NONE");
+
+        List<String> urls = Arrays.asList("url1");
+
+        Health health = this.healthUtil.callEndpoint(true, dataProject, urls,
+            TypeHealthEnum.VALIDATE_RESPONSE_STATUS_CODE_OK, Duration.ofHours(Long.valueOf(0)), Duration.ofHours(Long.valueOf(0)));
+
+        assertNotNull(health);
+    }
+
+    @Test
+    public void callEndpointTest3 () {
+        Map<String, String> dataProject = new HashMap<>();
+        dataProject.put("projectService", "ICMCLCWB");
+        dataProject.put("usernameService", "user");
+        dataProject.put("passwordService", "pass");
+        dataProject.put("authMethodService", "BASIC");
+
+        List<String> urls = Arrays.asList("url1");
+
+        Health health = this.healthUtil.callEndpoint(true, dataProject, urls,
+            TypeHealthEnum.AMIGA_HEALTH_STATUS_UP, Duration.ofHours(Long.valueOf(0)), Duration.ofHours(Long.valueOf(0)));
+
+        assertNotNull(health);
+    }
+
+}
