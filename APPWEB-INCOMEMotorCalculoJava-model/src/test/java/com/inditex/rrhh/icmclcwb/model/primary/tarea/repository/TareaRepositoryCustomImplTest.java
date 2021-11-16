0641c7cc31d7a4e4c63ed233d7470287ad95f590
Javier Estévez <jestevezv@vectoritcgroup.com>
2021-11-16 13:46:45 +0100

    Corrección consulta parámetro idEstado

--- APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaRepositoryCustomImplTest.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaRepositoryCustomImplTest.java
@@ -326,6 +326,11 @@ class TareaRepositoryCustomImplTest {
 
         final Integer result = this.tareaRepositoryCustom.totalTareasConsolidadesSinAjusteComision();
 
+        // Parámetros de la consulta: limit, idEstado
+        final MapSqlParameterSource params = this.paramsCaptor.getValue();
+        assertTrue(params.hasValue("idEstado"));
+        assertEquals(EstadoTareaEnum.FINALIZADO_SIN_ERRORES.getId(), params.getValue("idEstado"));
+
         assertEquals(total, result);
 
     }
