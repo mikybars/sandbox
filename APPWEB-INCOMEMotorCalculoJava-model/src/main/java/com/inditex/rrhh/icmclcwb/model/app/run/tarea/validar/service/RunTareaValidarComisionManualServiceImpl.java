5c9cc48f54b5764f068a9d8cdee92dfc6092be66
Javier Estévez <javier.estevez@softtek.com>
2022-03-03 15:10:26 +0100

    RunTareaAmbitoValidarComisionManualService

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/RunTareaValidarComisionManualServiceImpl.java
@@ -0,0 +1,35 @@
+package com.inditex.rrhh.icmclcwb.model.app.run.tarea.validar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import com.inditex.rrhh.icmclcwb.api.app.dto.ValidacionDto;
+import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.EstadoTareaFaseAccionEnum;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaFaseAccionDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.service.TareaFaseAccionService;
+import com.inditex.rrhh.icmclcwb.model.app.calcular.RunPrevalidar;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component("comisionManualV1")
+@Validated
+public class RunTareaValidarComisionManualServiceImpl implements RunPrevalidar {
+
+  @Autowired
+  private TareaFaseAccionService tareaFaseAccionService;
+
+  @Override
+  public CompletableFuture<List<ValidacionDto>> execute(
+      @NotNull @Valid final RunTareaDto runTarea,
+      @NotNull @Valid final TareaFaseAccionDto tareaFaseAccion) {
+    this.tareaFaseAccionService.updateFechaFinAndEstado(tareaFaseAccion,
+        EstadoTareaFaseAccionEnum.NO_EJECUTADA.getDto());
+    return CompletableFuture.completedFuture(new ArrayList<>());
+  }
+}
