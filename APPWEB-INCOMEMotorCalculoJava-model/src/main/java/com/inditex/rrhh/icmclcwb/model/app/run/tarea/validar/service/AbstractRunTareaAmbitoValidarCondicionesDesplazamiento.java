e1908b96ae20f8eafc39ceafd286e4491993ece3
marcelorf <marcelorf@ext.inditex.com>
2022-03-28 14:23:00 +0200

    Añadido test en desplazamiento

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/AbstractRunTareaAmbitoValidarCondicionesDesplazamiento.java
@@ -0,0 +1,41 @@
+package com.inditex.rrhh.icmclcwb.model.app.run.tarea.validar.service;
+/*
+ * Copyright (c) 2022. Inditex
+ */
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import com.inditex.rrhh.icmclcwb.api.app.dto.IdPersonaLocalCondicionesDto;
+import com.inditex.rrhh.icmclcwb.api.app.dto.IdPersonaLocalDto;
+import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
+import com.inditex.rrhh.icmclcwb.model.app.util.AsyncUtils;
+import com.inditex.rrhh.icmclcwb.model.primary.repository.PrimaryTemporaryTableRepositoryCustom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AbstractRunTareaAmbitoValidarCondicionesDesplazamiento {
+
+  @Autowired
+  private PrimaryTemporaryTableRepositoryCustom primaryTemporaryTableRepositoryCustom;
+
+  List<IdPersonaLocalDto> getIdPersonaLocalDtos(final RunTareaDto runTareaDto, final TareaDto tareaDto,
+      final CompletableFuture<List<IdPersonaLocalCondicionesDto>> cfCondicionesDesplazamiento) {
+    final List<IdPersonaLocalDto> desplazamientoValidationResult;
+    final List<IdPersonaLocalCondicionesDto> condicionesDesplazamiento = AsyncUtils
+        .get(cfCondicionesDesplazamiento);
+
+    this.primaryTemporaryTableRepositoryCustom.createTempComisDesplazamiento();
+    this.primaryTemporaryTableRepositoryCustom.insertTempComisDesplazamiento(condicionesDesplazamiento);
+
+    this.primaryTemporaryTableRepositoryCustom.desactivaFechasSolapadas();
+    this.primaryTemporaryTableRepositoryCustom.reactivaFechasSolapadas(tareaDto);
+
+    desplazamientoValidationResult = this.primaryTemporaryTableRepositoryCustom
+        .validateTempComisDesplazamiento(runTareaDto.getTarea());
+
+    this.primaryTemporaryTableRepositoryCustom.deleteTempComisDesplazamiento();
+    return desplazamientoValidationResult;
+  }
+}
--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/RunTareaAmbitoValidarCondicionesDesplazamientoServiceImpl.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/RunTareaAmbitoValidarCondicionesDesplazamientoServiceImpl.java
@@ -30,7 +30,7 @@ import org.springframework.validation.annotation.Validated;
 
 @Service
 @Validated
-public class RunTareaAmbitoValidarCondicionesDesplazamientoServiceImpl
+public class RunTareaAmbitoValidarCondicionesDesplazamientoServiceImpl extends AbstractRunTareaAmbitoValidarCondicionesDesplazamiento
     implements RunTareaAmbitoValidarCondicionesDesplazamientoService {
 
   @Autowired
@@ -67,16 +67,7 @@ public class RunTareaAmbitoValidarCondicionesDesplazamientoServiceImpl
       final List<IdPersonaLocalCondicionesDto> condicionesDesplazamiento = AsyncUtils
           .get(cfCondicionesDesplazamiento);
 
-      this.primaryTemporaryTableRepositoryCustom.createTempComisDesplazamiento();
-      this.primaryTemporaryTableRepositoryCustom.insertTempComisDesplazamiento(condicionesDesplazamiento);
-
-      this.primaryTemporaryTableRepositoryCustom.desactivaFechasSolapadas();
-      this.primaryTemporaryTableRepositoryCustom.reactivaFechasSolapadas(tareaDto);
-
-      desplazamientoValidationResult = this.primaryTemporaryTableRepositoryCustom
-          .validateTempComisDesplazamiento(runTareaDto.getTarea());
-
-      this.primaryTemporaryTableRepositoryCustom.deleteTempComisDesplazamiento();
+      desplazamientoValidationResult = this.getIdPersonaLocalDtos(runTareaDto, tareaDto, cfCondicionesDesplazamiento);
 
     } catch (final Exception e) {
       this.tareaFaseAccionService.updateFechaFinAndEstado(tareaFaseAccion,
