e0d650949c4ff84d158cc9ba9c7d1a615f7c2b9e
Javier Estévez <javier.estevez@softtek.com>
2022-04-04 12:08:39 +0200

    Eliminación de beans, repositories y consultas de Por Venta Simplificado

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package com.inditex.rrhh.icmclcwb.model.primary.tarea.repository;
-
-public interface TareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeDesplazamientoBaseV1RepositoryCustom
-    extends TareaCalculoAlgoritmoBaseRepositoryCustom {
-
-}
--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeV1RepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package com.inditex.rrhh.icmclcwb.model.primary.tarea.repository;
-
-public interface TareaCalculoAlgoritmoPorVentaSimplificadoPorcentajeV1RepositoryCustom
-    extends TareaCalculoAlgoritmoBaseRepositoryCustom {
-
-}
