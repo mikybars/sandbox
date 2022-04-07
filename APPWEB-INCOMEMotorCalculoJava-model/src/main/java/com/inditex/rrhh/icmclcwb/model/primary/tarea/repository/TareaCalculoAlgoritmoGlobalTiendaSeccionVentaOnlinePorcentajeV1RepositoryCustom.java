a20acba5b53c66175de397b33130e72cb3b9fe0e
Javier Estévez <javier.estevez@softtek.com>
2022-04-07 14:55:03 +0200

    Global Sección Venta Online

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoAlgoritmoGlobalTiendaSeccionVentaOnlinePorcentajeV1RepositoryCustom.java
@@ -0,0 +1,6 @@
+package com.inditex.rrhh.icmclcwb.model.primary.tarea.repository;
+
+public interface TareaCalculoAlgoritmoGlobalTiendaSeccionVentaOnlinePorcentajeV1RepositoryCustom
+    extends TareaCalculoAlgoritmoBaseRepositoryCustom {
+
+}
