2b32373f3ad165e901b0e03f14b66b1d3ae86aff
Javier Estévez <javier.estevez@softtek.com>
2022-04-12 14:13:04 +0200

    Enganche del algoritmo directo venta venta online

---
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/calcular/directoventaonline/v1/DirectoVentaVentaOnlinePorcentajeDesplazamientoBaseV1RunAlgoritmo.java
@@ -0,0 +1,29 @@
+package com.inditex.rrhh.icmclcwb.model.app.calcular.directoventaonline.v1;/*
+ * Copyright (c) 2022.  Inditex
+ */
+
+import com.inditex.rrhh.icmclcwb.model.app.calcular.AbstractV1RunAlgoritmo;
+import com.inditex.rrhh.icmclcwb.model.app.calcular.RunAlgoritmo;
+import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaCalculoAlgoritmoBaseRepositoryCustom;
+import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaCalculoAlgoritmoDirectoVentaVentaOnlinePorcentajeDesplazamientoBaseV1RepositoryCustom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("directoVentaVentaOnlinePorcentajeDesplazamientoBaseV1RunAlgoritmo")
+public class DirectoVentaVentaOnlinePorcentajeDesplazamientoBaseV1RunAlgoritmo extends AbstractV1RunAlgoritmo implements RunAlgoritmo {
+
+    @Autowired
+    private TareaCalculoAlgoritmoDirectoVentaVentaOnlinePorcentajeDesplazamientoBaseV1RepositoryCustom
+        tareaCalculoAlgoritmoDirectoVentaVentaOnlinePorcentajeDesplazamientoBaseV1RepositoryCustom;
+
+    @Override
+    protected TareaCalculoAlgoritmoBaseRepositoryCustom getTareaCalculoAlgoritmoRespositoryCustom() {
+        return this.tareaCalculoAlgoritmoDirectoVentaVentaOnlinePorcentajeDesplazamientoBaseV1RepositoryCustom;
+    }
+
+    @Override
+    protected String getAlgoritmoName() {
+        return "DirectoVentaVentaOnlinePorcentajeDesplazamientoBaseV1RunAlgoritmo";
+    }
+}
