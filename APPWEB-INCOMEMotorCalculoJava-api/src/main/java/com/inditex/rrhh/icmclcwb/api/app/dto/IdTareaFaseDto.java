74f323a87f986e2fd42746b0db241ec76047edc4
albertoggu <albertoggu@ext.inditex.com>
2022-05-12 09:36:04 +0200

    Añadir a limpieza las tablas TAREA_FASE, TAREA_FASE_ACCION y TAREA_FASE_ACCION_DATO

---
+++ APPWEB-INCOMEMotorCalculoJava-api/src/main/java/com/inditex/rrhh/icmclcwb/api/app/dto/IdTareaFaseDto.java
@@ -0,0 +1,26 @@
+package com.inditex.rrhh.icmclcwb.api.app.dto;
+
+/*
+ * Copyright (c) 2022. Inditex
+ */
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class IdTareaFaseDto implements Serializable {
+
+  private static final long serialVersionUID = 5877004626774810613L;
+
+  @NotBlank
+  private String idTareaFase;
+
+}
