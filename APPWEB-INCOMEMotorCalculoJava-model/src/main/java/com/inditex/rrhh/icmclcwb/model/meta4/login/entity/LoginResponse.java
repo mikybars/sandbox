dc0307bdfea8702b49e7cde4061ef099bd20966a
Javier Estévez <javier.estevez@softtek.com>
2022-05-25 08:56:57 +0200

    Correcciones en la regeneración

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/meta4/login/entity/LoginResponse.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/meta4/login/entity/LoginResponse.java
@@ -169,9 +169,8 @@ public class LoginResponse implements Serializable, Cloneable, CopyTo2, Equals2,
         if (loginReturnShouldBeCopiedAndSet == Boolean.TRUE) {
           final M4LoginOutput sourceLoginReturn;
           sourceLoginReturn = this.getLoginReturn();
-          final M4LoginOutput copyLoginReturn =
-              ((M4LoginOutput) strategy.copy(LocatorUtils.property(locator, "loginReturn", sourceLoginReturn),
-                  sourceLoginReturn, (this.loginReturn != null)));
+          final M4LoginOutput copyLoginReturn = ((M4LoginOutput) strategy
+              .copy(LocatorUtils.property(locator, "loginReturn", sourceLoginReturn), sourceLoginReturn, (this.loginReturn != null)));
           copy.setLoginReturn(copyLoginReturn);
         } else {
           if (loginReturnShouldBeCopiedAndSet == Boolean.FALSE) {
