4697d9175c0f3b348677e33c31055cf1dd998024
albertoggu <albertoggu@ext.inditex.com>
2022-07-12 09:02:05 +0200

    actualizaciones

--- APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/meta4/pool/Meta4ClientExpiration.java
+++ APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/meta4/pool/Meta4ClientExpiration.java
@@ -3,7 +3,7 @@ package com.inditex.rrhh.icmclcwb.model.meta4.pool;
 import com.inditex.rrhh.icmclcwb.model.meta4.icmwscalcincome.entity.IcmWsCalcIncomeService;
 import com.inditex.rrhh.icmclcwb.model.meta4.login.entity.LoginService;
 
-import net.logstash.logback.encoder.org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stormpot.Expiration;
@@ -14,19 +14,19 @@ public class Meta4ClientExpiration implements Expiration<Meta4ClientPoolable> {
   private static final Logger log = LoggerFactory.getLogger(Meta4ClientExpiration.class);
 
   @Override
-  public boolean hasExpired(SlotInfo<? extends Meta4ClientPoolable> info) {
+  public boolean hasExpired(final SlotInfo<? extends Meta4ClientPoolable> info) {
     boolean expired = true;
     boolean login = false;
     String session = StringUtils.EMPTY;
     try {
-      Meta4ClientPoolable poolable = info.getPoolable();
+      final Meta4ClientPoolable poolable = info.getPoolable();
       session = poolable.getSession().getId();
       login = poolable.getSession().isLogin();
-      LoginService loginService = poolable.getLoginService();
-      IcmWsCalcIncomeService icmWsCalcIncomeService = poolable.getIcmWsCalcIncomeService();
+      final LoginService loginService = poolable.getLoginService();
+      final IcmWsCalcIncomeService icmWsCalcIncomeService = poolable.getIcmWsCalcIncomeService();
       if (login && StringUtils.isNotBlank(session) && loginService != null && icmWsCalcIncomeService != null) {
-        int retrieveM4SessionLogin = loginService.retrieveM4Session(session);
-        int retrieveM4SessionIcmWsCalcIncome = icmWsCalcIncomeService.retrieveM4Session(session);
+        final int retrieveM4SessionLogin = loginService.retrieveM4Session(session);
+        final int retrieveM4SessionIcmWsCalcIncome = icmWsCalcIncomeService.retrieveM4Session(session);
         if (retrieveM4SessionLogin == 0 && retrieveM4SessionIcmWsCalcIncome == 0) {
           expired = false;
         }
