31d0c125c4d3e81d33ea898ffd42c1d0534e5bae
albertoggu <albertoggu@ext.inditex.com>
2023-10-18 12:44:37 +0200

    [ICMCLCWB-1394] Javaformat

--- code/APPWEB-INCOMEMotorCalculoJava-api/src/test/java/com/inditex/rrhh/icmclcwb/api/app/proceso/TipoProcesoEnumTest.java
+++ code/APPWEB-INCOMEMotorCalculoJava-api/src/test/java/com/inditex/rrhh/icmclcwb/api/app/proceso/TipoProcesoEnumTest.java
@@ -6,20 +6,20 @@ import org.junit.jupiter.api.Test;
 
 class TipoProcesoEnumTest {
 
-    @Test
-    void ofTest() {
-        assertEquals(TipoProcesoEnum.CONFIRMACION, TipoProcesoEnum.of(1));
-        assertEquals(TipoProcesoEnum.EXPORTACION, TipoProcesoEnum.of(2));
-        assertEquals(TipoProcesoEnum.ANULACION_CONFIRMACION, TipoProcesoEnum.of(3));
-        assertEquals(TipoProcesoEnum.ANULACION_EXPORTACION, TipoProcesoEnum.of(4));
-    }
+  @Test
+  void ofTest() {
+    assertEquals(TipoProcesoEnum.CONFIRMACION, TipoProcesoEnum.of(1));
+    assertEquals(TipoProcesoEnum.EXPORTACION, TipoProcesoEnum.of(2));
+    assertEquals(TipoProcesoEnum.ANULACION_CONFIRMACION, TipoProcesoEnum.of(3));
+    assertEquals(TipoProcesoEnum.ANULACION_EXPORTACION, TipoProcesoEnum.of(4));
+  }
 
-    @Test
-    void ofDtoTest() {
-        assertEquals(TipoProcesoEnum.CONFIRMACION, TipoProcesoEnum.of(TipoProcesoEnum.CONFIRMACION.getDto()));
-        assertEquals(TipoProcesoEnum.EXPORTACION, TipoProcesoEnum.of(TipoProcesoEnum.EXPORTACION.getDto()));
-        assertEquals(TipoProcesoEnum.ANULACION_CONFIRMACION, TipoProcesoEnum.of(TipoProcesoEnum.ANULACION_CONFIRMACION.getDto()));
-        assertEquals(TipoProcesoEnum.ANULACION_EXPORTACION, TipoProcesoEnum.of(TipoProcesoEnum.ANULACION_EXPORTACION.getDto()));
-    }
+  @Test
+  void ofDtoTest() {
+    assertEquals(TipoProcesoEnum.CONFIRMACION, TipoProcesoEnum.of(TipoProcesoEnum.CONFIRMACION.getDto()));
+    assertEquals(TipoProcesoEnum.EXPORTACION, TipoProcesoEnum.of(TipoProcesoEnum.EXPORTACION.getDto()));
+    assertEquals(TipoProcesoEnum.ANULACION_CONFIRMACION, TipoProcesoEnum.of(TipoProcesoEnum.ANULACION_CONFIRMACION.getDto()));
+    assertEquals(TipoProcesoEnum.ANULACION_EXPORTACION, TipoProcesoEnum.of(TipoProcesoEnum.ANULACION_EXPORTACION.getDto()));
+  }
 
 }
--- code/APPWEB-INCOMEMotorCalculoJava-api/src/test/java/com/inditex/rrhh/icmclcwb/api/app/proceso/TipoSistemaDestinoEnumTest.java
+++ code/APPWEB-INCOMEMotorCalculoJava-api/src/test/java/com/inditex/rrhh/icmclcwb/api/app/proceso/TipoSistemaDestinoEnumTest.java
@@ -6,18 +6,18 @@ import org.junit.jupiter.api.Test;
 
 class TipoSistemaDestinoEnumTest {
 
-    @Test
-    void ofTest() {
-        assertEquals(TipoSistemaDestinoEnum.SIL, TipoSistemaDestinoEnum.of(1));
-        assertEquals(TipoSistemaDestinoEnum.AS400, TipoSistemaDestinoEnum.of(2));
-        assertEquals(TipoSistemaDestinoEnum.SOLUCION_GLOBAL, TipoSistemaDestinoEnum.of(3));
-    }
+  @Test
+  void ofTest() {
+    assertEquals(TipoSistemaDestinoEnum.SIL, TipoSistemaDestinoEnum.of(1));
+    assertEquals(TipoSistemaDestinoEnum.AS400, TipoSistemaDestinoEnum.of(2));
+    assertEquals(TipoSistemaDestinoEnum.SOLUCION_GLOBAL, TipoSistemaDestinoEnum.of(3));
+  }
 
-    @Test
-    void ofDtoTest() {
-        assertEquals(TipoSistemaDestinoEnum.SIL, TipoSistemaDestinoEnum.of(TipoSistemaDestinoEnum.SIL.getDto()));
-        assertEquals(TipoSistemaDestinoEnum.AS400, TipoSistemaDestinoEnum.of(TipoSistemaDestinoEnum.AS400.getDto()));
-        assertEquals(TipoSistemaDestinoEnum.SOLUCION_GLOBAL, TipoSistemaDestinoEnum.of(TipoSistemaDestinoEnum.SOLUCION_GLOBAL.getDto()));
-    }
+  @Test
+  void ofDtoTest() {
+    assertEquals(TipoSistemaDestinoEnum.SIL, TipoSistemaDestinoEnum.of(TipoSistemaDestinoEnum.SIL.getDto()));
+    assertEquals(TipoSistemaDestinoEnum.AS400, TipoSistemaDestinoEnum.of(TipoSistemaDestinoEnum.AS400.getDto()));
+    assertEquals(TipoSistemaDestinoEnum.SOLUCION_GLOBAL, TipoSistemaDestinoEnum.of(TipoSistemaDestinoEnum.SOLUCION_GLOBAL.getDto()));
+  }
 
 }
