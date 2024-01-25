b9285010eb822d24855829dc6fdb8a91e24366f1
albertoggu <albertoggu@ext.inditex.com>
2024-01-25 10:51:36 +0100

    [ICMCLCWB-1539] tests

--- code/APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/RunTareaAmbitoValidarExternosBrasilServiceImplTest.java
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/RunTareaAmbitoValidarExternosBrasilServiceImplTest.java
@@ -10,16 +10,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import com.inditex.aqsw.framework.test.randomizer.Random;
+import com.inditex.aqsw.framework.test.randomizer.RandomizerExtension;
 import com.inditex.rrhh.icmclcwb.api.app.ComisClaseEmpleadoEnum;
 import com.inditex.rrhh.icmclcwb.api.app.async.service.ComisAsyncService;
 import com.inditex.rrhh.icmclcwb.api.app.dto.IdPersonaLocalExternaDto;
 import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.service.IncomeMetaService;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.ReglaEmpleadoExternoMeta4RequestDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaAmbitoDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaFaseAccionDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.service.ReglaEmpleadoExternoMeta4Service;
+import com.inditex.rrhh.icmclcwb.model.app.mapper.IdPersonaLocalExternaMapper;
 import com.inditex.rrhh.icmclcwb.model.app.run.tarea.validar.mapper.ValidacionMapper;
+import com.inditex.rrhh.icmclcwb.model.app.tarea.mapper.ReglaEmpleadoExternoMeta4Mapper;
 import com.inditex.rrhh.icmclcwb.model.app.tarea.mapper.TareaPersonaExternaMapper;
 import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaPersonaExternaRepositoryCustom;
+import com.inditex.rrhh.icmclcwb.rest.client.dto.EmpleadoExternoDTO;
+import com.inditex.rrhh.icmclcwb.rest.client.dto.ExternosRequestDTO;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,7 +36,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith({SpringExtension.class, RandomizerExtension.class})
 public class RunTareaAmbitoValidarExternosBrasilServiceImplTest {
 
   @Mock
@@ -42,11 +51,25 @@ public class RunTareaAmbitoValidarExternosBrasilServiceImplTest {
   @Mock
   private ValidacionMapper validacionMapper;
 
+  @Mock
+  private IncomeMetaService incomeMetaService;
+
+  @Mock
+  private ReglaEmpleadoExternoMeta4Service reglaEmpleadoExternoMeta4Service;
+
+  @Mock
+  private ReglaEmpleadoExternoMeta4Mapper reglaEmpleadoExternoMeta4Mapper;
+
+  @Mock
+  private IdPersonaLocalExternaMapper idPersonaLocalExternaMapper;
+
   @InjectMocks
   private RunTareaAmbitoValidarExternosBrasilServiceImpl runTareaAmbitoValidarExternosBrasilService;
 
   @Test
-  public void executeTest() {
+  public void executeTest(@Random final ReglaEmpleadoExternoMeta4RequestDto request, @Random final ExternosRequestDTO externosRequestDTO,
+      @Random(size = 2, type = EmpleadoExternoDTO.class) final List<EmpleadoExternoDTO> externos,
+      @Random(size = 2, type = IdPersonaLocalExternaDto.class) final List<IdPersonaLocalExternaDto> idPersonaLocalExternaDtos) {
 
     final RunTareaDto runTareaDto = new RunTareaDto();
     final TareaDto tareaDto = new TareaDto();
@@ -61,6 +84,18 @@ public class RunTareaAmbitoValidarExternosBrasilServiceImplTest {
 
     when(this.comisAsyncService.findExternosByClase(any(RunTareaDto.class), any(TareaAmbitoDto.class), any(
         ComisClaseEmpleadoEnum.class))).thenReturn(cf);
+
+    when(this.reglaEmpleadoExternoMeta4Service.getReglasEmpleadoExternoMeta4ActivasByCclIdOrigen(any(String.class), any(String.class)))
+        .thenReturn(request);
+
+    when(this.reglaEmpleadoExternoMeta4Mapper.reglaEmpleadoExternoMeta4RequestDtotoExternosRequestDto(request))
+        .thenReturn(externosRequestDTO);
+
+    when(this.incomeMetaService.getEmpleadosExternosExcluidosDenominador(externosRequestDTO))
+        .thenReturn(externos);
+
+    when(this.idPersonaLocalExternaMapper.empleadoExternoDTOtoIdPersonaLocalExternaDto(externos))
+        .thenReturn(idPersonaLocalExternaDtos);
 
     this.runTareaAmbitoValidarExternosBrasilService.execute(runTareaDto, tareaAmbitoDto, tareaFaseAccionDto);
 
