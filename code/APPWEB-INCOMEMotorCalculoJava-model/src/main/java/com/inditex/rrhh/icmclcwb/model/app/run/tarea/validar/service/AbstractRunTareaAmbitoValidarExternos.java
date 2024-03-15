4697ef1e4397809f5169f5b556ee798bd1029357
albertoggu <albertoggu@ext.inditex.com>
2024-03-15 16:31:16 +0100

    [ICMCLCWB-1539] Algoritmo de aplanado de fechas

--- code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/AbstractRunTareaAmbitoValidarExternos.java
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/AbstractRunTareaAmbitoValidarExternos.java
@@ -1,7 +1,16 @@
 package com.inditex.rrhh.icmclcwb.model.app.run.tarea.validar.service;
 
+import static java.util.stream.Collectors.groupingBy;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 import com.inditex.rrhh.icmclcwb.api.app.dto.IdPersonaLocalExternaDto;
@@ -60,17 +69,61 @@ public abstract class AbstractRunTareaAmbitoValidarExternos {
 
     final List<IdPersonaLocalExternaDto> externos = AsyncUtils.get(cfExternos);
 
-    final ReglaEmpleadoExternoMeta4RequestDto request =
-        this.reglaEmpleadoExternoMeta4Service.getReglasEmpleadoExternoMeta4ActivasByCclIdOrigen(tareaAmbito.getCclIdOrigen(),
-            runTarea.getTarea().getStdIdLegEnt());
+    final Optional<ReglaEmpleadoExternoMeta4RequestDto> request =
+        Optional.ofNullable(
+            this.reglaEmpleadoExternoMeta4Service.getReglasEmpleadoExternoMeta4ActivasByCclIdOrigen(tareaAmbito.getCclIdOrigen(),
+                runTarea.getTarea().getStdIdLegEnt()));
 
-    if (request != null) {
-      final ExternosRequestDTO req = this.reglaEmpleadoExternoMeta4Mapper.reglaEmpleadoExternoMeta4RequestDtotoExternosRequestDto(request);
+    if (request.isPresent()) {
+      final ExternosRequestDTO req =
+          this.reglaEmpleadoExternoMeta4Mapper.reglaEmpleadoExternoMeta4RequestDtotoExternosRequestDto(request.get());
       req.setFechaDesde(runTarea.getTarea().getFechaInicioPeriodo());
       req.setFechaHasta(runTarea.getTarea().getFechaFinPeriodo());
       final List<EmpleadoExternoDTO> excluidosMeta4 = this.incomeMetaService.getEmpleadosExternosExcluidosDenominador(req);
 
-      externos.addAll(this.idPersonaLocalExternaMapper.empleadoExternoDTOtoIdPersonaLocalExternaDto(excluidosMeta4));
+      // Agrupamos por empleado
+      final Map<String, List<EmpleadoExternoDTO>> excluidosMap =
+          excluidosMeta4.stream().collect(groupingBy(EmpleadoExternoDTO::getIdPersonaLocal));
+
+      // Iteramos por cada empleado
+      for (final List<EmpleadoExternoDTO> listaIntervalosEmpExterno : excluidosMap.values()) {
+        final Set<LocalDate> fechasTrabajadas = new HashSet<>();
+
+        // Iteramos por cada intervalo perteneciente al empleado guardando las fechas en las que trabajo
+        listaIntervalosEmpExterno.forEach(intervalo -> {
+          LocalDate iterativeDate = intervalo.getFechaDesde();
+          while (iterativeDate.isBefore(intervalo.getFechaHasta()) || iterativeDate.isEqual(intervalo.getFechaHasta())) {
+            fechasTrabajadas.add(iterativeDate);
+            iterativeDate = iterativeDate.plusDays(1);
+          }
+        });
+
+        // Ordenamos las fechas de manera ascendente
+        final List<LocalDate> fechasTrabajadasSortedList = new ArrayList<>(fechasTrabajadas);
+        Collections.sort(fechasTrabajadasSortedList);
+
+        // Algoritmo para el cálculo de intervalos comprometidos por las fechas trabajadas
+        LocalDate prevDate = null;
+        LocalDate startDate = null;
+
+        for (int i = 0; i <= fechasTrabajadasSortedList.size(); i++) {
+          if (prevDate != null) {
+            if (i == fechasTrabajadasSortedList.size() || ChronoUnit.DAYS.between(prevDate, fechasTrabajadasSortedList.get(i)) > 1) {
+              externos.add(IdPersonaLocalExternaDto.builder()
+                  .idPersonaLocal(listaIntervalosEmpExterno.stream().findAny().get().getIdPersonaLocal())
+                  .fechaDesde(startDate)
+                  .fechaHasta(prevDate).build());
+              startDate = null;
+            }
+          }
+          if (startDate == null && i < fechasTrabajadasSortedList.size()) {
+            startDate = fechasTrabajadasSortedList.get(i);
+          }
+          if (i < fechasTrabajadasSortedList.size()) {
+            prevDate = fechasTrabajadasSortedList.get(i);
+          }
+        }
+      }
     }
 
     this.tareaPersonaExternaRepositoryCustom
