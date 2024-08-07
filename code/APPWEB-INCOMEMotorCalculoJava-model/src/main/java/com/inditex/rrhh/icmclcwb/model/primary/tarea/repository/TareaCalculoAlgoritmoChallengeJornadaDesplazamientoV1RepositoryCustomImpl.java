76a77cb47d3be500cb537c19cbff4ade2e3c1796
marceloitx <marcelorf@ext.inditex.com>
2024-08-07 11:33:40 +0200

    [INCOME-484] - Refactor

--- code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoAlgoritmoChallengeJornadaDesplazamientoV1RepositoryCustomImpl.java
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoAlgoritmoChallengeJornadaDesplazamientoV1RepositoryCustomImpl.java
@@ -1,21 +1,12 @@
 package com.inditex.rrhh.icmclcwb.model.primary.tarea.repository;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import com.inditex.rrhh.icmclcwb.api.app.calcular.service.TipoDatoService;
 import com.inditex.rrhh.icmclcwb.api.app.dto.IdPersonaLocalDto;
-import com.inditex.rrhh.icmclcwb.api.app.tarea.TipoDatoEnum;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.service.TareaCalculoPersonaService;
-import com.inditex.rrhh.icmclcwb.api.app.util.AppConstants;
-import com.inditex.rrhh.icmclcwb.api.app.util.SqlPrimaryConstants;
 import com.inditex.rrhh.icmclcwb.dto.AlgoritmoDTO;
-import com.inditex.rrhh.icmclcwb.dto.TipoCalculoDTO;
-import com.inditex.rrhh.icmclcwb.dto.TipoComisionDTO;
-import com.inditex.rrhh.icmclcwb.model.app.util.TimeUtils;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +15,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class TareaCalculoAlgoritmoChallengeJornadaDesplazamientoV1RepositoryCustomImpl
-    extends AbstractTareaCalculoAlgoritmoBaseRepositoryCustom
+    extends TareaCalculoAlgoritmoChallengeJornadaV1RepositoryCommon
     implements TareaCalculoAlgoritmoChallengeJornadaDesplazamientoV1RepositoryCustom {
 
   @Value("#{calculoPrimaryQuery['TareaCalculoAlgoritmoBaseRepository.calcular.insert']} "
@@ -43,39 +34,6 @@ public class TareaCalculoAlgoritmoChallengeJornadaDesplazamientoV1RepositoryCust
 
   @Autowired
   private TipoDatoService tipoDatoService;
-
-  @Override
-  protected Map<String, Object> getMapValues(final AlgoritmoDTO algoritmo, final TareaDto tarea, final IdPersonaLocalDto persona) {
-    final Map<String, Object> map = new HashMap<>();
-    if (tarea != null) {
-      map.put(SqlPrimaryConstants.SQL_PARAM_FECHA_INICIO_PERIODO, TimeUtils.toDate(tarea.getFechaInicioPeriodo()));
-      map.put(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, tarea.getId());
-    }
-    if (persona != null) {
-      map.put(SqlPrimaryConstants.SQL_PARAM_CCL_ID_PERSON, persona.getIdPersonaLocal());
-      map.put(SqlPrimaryConstants.SQL_PARAM_STD_OR_HR_PERIOD, persona.getStdOrHrPeriod());
-    }
-    map.put(SqlPrimaryConstants.SQL_PARAM_ID_ALGORITMO, algoritmo.getId());
-    map.put(SqlPrimaryConstants.SQL_PARAM_COMISIONABLE, SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE);
-    map.put(SqlPrimaryConstants.SQL_PARAM_CALCULA, SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE);
-    map.put(SqlPrimaryConstants.SQL_PARAM_ACTIVO, SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE);
-    map.put(SqlPrimaryConstants.SQL_PARAM_INACTIVO, SqlPrimaryConstants.SQL_VALUE_BOOLEAN_FALSE);
-    map.put(SqlPrimaryConstants.SQL_PARAM_ID_TIPO_DATO_JORNADA_LOCALIZACION, TipoDatoEnum.JORNADA_LOCALIZACION.getId());
-    map.put(SqlPrimaryConstants.SQL_PARAM_ID_SECCION, AppConstants.SECCION_4);
-
-    map.put(SqlPrimaryConstants.SQL_PARAM_IDS_TIPOS_COMISION,
-        algoritmo.getTipoComision().stream().map(TipoComisionDTO::getId).collect(Collectors.toList()));
-    map.put(SqlPrimaryConstants.SQL_PARAM_IDS_TIPOS_CALCULO,
-        algoritmo.getTipoCalculo().stream().map(TipoCalculoDTO::getId).collect(Collectors.toList()));
-    map.put(SqlPrimaryConstants.SQL_PARAM_ES_DESPLAZAMIENTO, algoritmo.getDesplazamiento()
-        ? SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE
-        : SqlPrimaryConstants.SQL_VALUE_BOOLEAN_FALSE);
-    map.put(SqlPrimaryConstants.SQL_PARAM_ES_DESPLAZAMIENTO_BASE, algoritmo.getDesplazamientoBase()
-        ? SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE
-        : SqlPrimaryConstants.SQL_VALUE_BOOLEAN_FALSE);
-
-    return map;
-  }
 
   @Override
   public List<IdPersonaLocalDto> ids(final AlgoritmoDTO algoritmo, final TareaDto tarea) {
