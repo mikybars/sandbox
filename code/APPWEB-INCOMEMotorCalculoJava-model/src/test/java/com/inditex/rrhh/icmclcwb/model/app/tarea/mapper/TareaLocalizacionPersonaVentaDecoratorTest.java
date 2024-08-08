3ae45de84edb331974fbbd44d4fe6e327c41a239
marceloitx <marcelorf@ext.inditex.com>
2024-08-08 13:09:47 +0200

    [INCOME-484] - Actualizacion dependencias y formatter

--- code/APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/tarea/mapper/TareaLocalizacionPersonaVentaDecoratorTest.java
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/app/tarea/mapper/TareaLocalizacionPersonaVentaDecoratorTest.java
@@ -3,7 +3,6 @@ package com.inditex.rrhh.icmclcwb.model.app.tarea.mapper;
 /*
  * Copyright (c) 2021. Inditex
  */
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
