69ac66b8516fd6ece6dd2c63b4936ad53122d3c1
Alejandro Gallego <alejandrogmori@ext.inditex.com>
2024-10-18 12:55:40 +0200

    fix(INCOME-974): apply amiga java format

--- code/APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/run/tarea/migrar/RunTareaMigrarAsyncServiceImplTest.java
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/run/tarea/migrar/RunTareaMigrarAsyncServiceImplTest.java
@@ -1,36 +1,39 @@
 package com.inditex.rrhh.icmclcwb.model.run.tarea.migrar;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.concurrent.CompletableFuture;
+
 import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.service.TareaMigrarService;
 import com.inditex.rrhh.icmclcwb.model.app.run.tarea.migrar.RunTareaMigrarAsyncServiceImpl;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.concurrent.CompletableFuture;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
-
 @ExtendWith(SpringExtension.class)
 class RunTareaMigrarAsyncServiceImplTest {
 
-    @Mock
-    private TareaMigrarService tareaMigrarService;
+  @Mock
+  private TareaMigrarService tareaMigrarService;
 
-    @InjectMocks
-    private RunTareaMigrarAsyncServiceImpl runTareaMigrarAsyncServiceImpl;
+  @InjectMocks
+  private RunTareaMigrarAsyncServiceImpl runTareaMigrarAsyncServiceImpl;
 
-    @Test
-    void testMigrarListCalculoComision() {
-        RunTareaDto runTareaDto = new RunTareaDto();
-        doNothing().when(tareaMigrarService).migrarCalculoComision(runTareaDto);
+  @Test
+  void testMigrarListCalculoComision() {
+    final RunTareaDto runTareaDto = new RunTareaDto();
+    doNothing().when(this.tareaMigrarService).migrarCalculoComision(runTareaDto);
 
-        CompletableFuture<Void> result = runTareaMigrarAsyncServiceImpl.migrarListCalculoComision(runTareaDto);
+    final CompletableFuture<Void> result = this.runTareaMigrarAsyncServiceImpl.migrarListCalculoComision(runTareaDto);
 
-        verify(tareaMigrarService, times(1)).migrarCalculoComision(runTareaDto);
-        assertTrue(result.isDone());
-    }
+    verify(this.tareaMigrarService, times(1)).migrarCalculoComision(runTareaDto);
+    assertTrue(result.isDone());
+  }
 }
