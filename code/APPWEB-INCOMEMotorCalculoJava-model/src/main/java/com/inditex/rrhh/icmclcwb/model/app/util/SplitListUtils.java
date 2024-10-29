14a0190e3e6f28e2a04a67aeb58808b71015130e
Alejandro Gallego <alejandrogmori@ext.inditex.com>
2024-10-29 16:13:22 +0100

    feat(INCOME-1034): add logs

--- code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/util/SplitListUtils.java
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/util/SplitListUtils.java
@@ -6,10 +6,12 @@ import java.util.stream.IntStream;
 import com.inditex.icmclcwb.commisioncalculation.model.v1.CommisionCalculationEvent;
 import com.inditex.icmclcwb.commisioncalculation.model.v1.CommisionCalculationEventList;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SplitListUtils {
 
   @Value("${app.envars.pipe.commission-calculation.message-max-size:5000}")
@@ -21,7 +23,7 @@ public class SplitListUtils {
   public List<CommisionCalculationEventList> splitList(CommisionCalculationEventList originalList) {
     final List<CommisionCalculationEvent> events = originalList.getEvents();
     final int totalSize = events.size();
-
+    log.info("Splitting list of size {} with limit {}", totalSize, this.maxSize);
     return IntStream.range(0, (totalSize + this.maxSize - 1) / this.maxSize)
         .mapToObj(i -> {
           final CommisionCalculationEventList subList = new CommisionCalculationEventList();
