d067d46a54c06d61fdf9d1591efe78d0cbbb5309
Alejandro Gallego <alejandrogmori@ext.inditex.com>
2024-11-25 17:08:33 +0100

    feat(INCOME-996): refactor external meta4 validation. Move to new independent task

---
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/app/run/tarea/validar/service/RunTareaValidarExternosMeta4ServiceImpl.java
@@ -0,0 +1,67 @@
+package com.inditex.rrhh.icmclcwb.model.app.run.tarea.validar.service;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import com.inditex.rrhh.icmclcwb.api.app.dto.ValidacionDto;
+import com.inditex.rrhh.icmclcwb.api.app.run.tarea.dto.RunTareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.run.tarea.validar.service.RunTareaAmbitoValidarExternosMeta4Service;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.EstadoTareaFaseAccionEnum;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaFaseAccionDto;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.service.AccionService;
+import com.inditex.rrhh.icmclcwb.api.app.tarea.service.TareaFaseAccionService;
+import com.inditex.rrhh.icmclcwb.model.app.calcular.RunPrevalidar;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component("externosM4V1")
+@Validated
+public class RunTareaValidarExternosMeta4ServiceImpl implements RunPrevalidar {
+
+  @Autowired
+  private TareaFaseAccionService tareaFaseAccionService;
+
+  @Autowired
+  private RunTareaAmbitoValidarExternosMeta4Service runTareaAmbitoValidarExternosMeta4Service;
+
+  @Autowired
+  private AccionService accionService;
+
+  @Override
+  public CompletableFuture<List<ValidacionDto>> execute(
+      @NotNull @Valid final RunTareaDto runTarea,
+      @NotNull @Valid final TareaFaseAccionDto tareaFaseAccion) {
+
+    final TareaDto tareaDto = runTarea.getTarea();
+    this.tareaFaseAccionService.updateFechaInicio(tareaFaseAccion);
+
+    final List<ValidacionDto> validaciones = runTarea.getTarea()
+        .getAmbito()
+        .stream()
+        .filter(a -> Boolean.TRUE.equals(
+            this.accionService.findByIdAccionAndIdOrigenAndStdIdLegEnt(tareaFaseAccion.getIdAccion(),
+                a.getCclIdOrigen(),
+                tareaDto.getStdIdLegEnt())))
+        .map(item -> this.runTareaAmbitoValidarExternosMeta4Service.execute(runTarea, item, tareaFaseAccion))
+        .collect(Collectors.toList());
+
+    if (validaciones.isEmpty()) {
+      this.tareaFaseAccionService.updateFechaFinAndEstado(tareaFaseAccion,
+          EstadoTareaFaseAccionEnum.NO_EJECUTADA.getDto());
+      return CompletableFuture.completedFuture(validaciones);
+    }
+    if (validaciones.stream()
+        .filter(e -> e.getResult().equals(Boolean.FALSE))
+        .toList()
+        .isEmpty()) {
+      this.tareaFaseAccionService.updateFechaFinAndEstado(tareaFaseAccion, EstadoTareaFaseAccionEnum.OK.getDto());
+    }
+    return CompletableFuture.completedFuture(validaciones);
+  }
+}
