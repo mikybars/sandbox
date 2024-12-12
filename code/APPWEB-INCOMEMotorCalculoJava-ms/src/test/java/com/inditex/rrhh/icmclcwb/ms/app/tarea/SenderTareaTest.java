8f19d7c50b80b1f60e8c96a69d4daff4570060af
danifhid <danielfh@ext.inditex.com>
2024-12-12 11:13:17 +0100

    INCOME-1024 Correction for test senderTarea

--- code/APPWEB-INCOMEMotorCalculoJava-ms/src/test/java/com/inditex/rrhh/icmclcwb/ms/app/tarea/SenderTareaTest.java
+++ code/APPWEB-INCOMEMotorCalculoJava-ms/src/test/java/com/inditex/rrhh/icmclcwb/ms/app/tarea/SenderTareaTest.java
@@ -5,23 +5,20 @@
 package com.inditex.rrhh.icmclcwb.ms.app.tarea;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.inditex.amigafwk.data.jms.JmsClient;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
 
-import jakarta.jms.Message;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jms.core.MessagePostProcessor;
 
 @ExtendWith(MockitoExtension.class)
 class SenderTareaTest {
@@ -41,37 +38,28 @@ class SenderTareaTest {
   void sendWithDelayWithPriorityTest() {
 
     final TareaDto tarea = Mockito.mock(TareaDto.class);
+    final TareaPriorityEnum priority = TareaPriorityEnum.MANUAL;
 
     this.senderTarea.sendWithDelayWithPriority(tarea, 1L, TareaPriorityEnum.MANUAL);
 
     verify(this.senderTarea, times(1))
         .sendWithDelayWithPriority(Mockito.any(TareaDto.class), Mockito.eq(1L), Mockito.eq(TareaPriorityEnum.MANUAL));
+    verify(this.tareaJmsClient).setPriority(priority.getPriority());
+    verify(this.tareaJmsClient).convertAndSend(any(TareaDto.class), any(MessagePostProcessor.class));
+
   }
 
   @Test
   void sendWithPriorityTest() {
     final TareaDto tarea = Mockito.mock(TareaDto.class);
+    final TareaPriorityEnum priority = TareaPriorityEnum.MANUAL;
+
     this.senderTarea.sendWithPriority(tarea, TareaPriorityEnum.MANUAL);
 
     verify(this.senderTarea, times(1))
         .sendWithPriority(Mockito.any(TareaDto.class), Mockito.eq(TareaPriorityEnum.MANUAL));
-  }
-
-  @Test
-  void sendWithDelayWithPriorityShouldSetDelayProperty() throws Exception {
-    final TareaDto tarea = new TareaDto();
-    final long delay = 5000L;
-    final TareaPriorityEnum priority = TareaPriorityEnum.MANUAL;
-
-    doAnswer((Answer<Void>) invocation -> {
-      final Message message = invocation.getArgument(1);
-      message.setLongProperty("_AMQ_SCHED_DELIVERY", delay);
-      return null;
-    }).when(this.tareaJmsClient).convertAndSend(eq(tarea), any());
-
-    this.senderTarea.sendWithDelayWithPriority(tarea, delay, priority);
-
     verify(this.tareaJmsClient).setPriority(priority.getPriority());
-    verify(this.tareaJmsClient).convertAndSend(eq(tarea), any());
+    verify(this.tareaJmsClient).convertAndSend(tarea);
   }
+
 }
