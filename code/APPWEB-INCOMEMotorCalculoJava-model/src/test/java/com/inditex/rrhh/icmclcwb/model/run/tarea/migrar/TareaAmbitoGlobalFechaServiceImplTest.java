a2779ba40aaf6912b543f84bac703eaa1ed60886
danifhid <danielfh@ext.inditex.com>
2024-12-26 10:40:48 +0100

    INCOME-1026 Correction for Sonar

--- code/APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/run/tarea/migrar/TareaAmbitoGlobalFechaServiceImplTest.java
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/test/java/com/inditex/rrhh/icmclcwb/model/run/tarea/migrar/TareaAmbitoGlobalFechaServiceImplTest.java
@@ -1,13 +1,17 @@
 package com.inditex.rrhh.icmclcwb.model.run.tarea.migrar;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Collections;
 
 import com.inditex.rrhh.icmclcwb.api.app.dto.PeriodoDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaAmbitoGlobalFechaDto;
 import com.inditex.rrhh.icmclcwb.api.app.tarea.dto.TareaDto;
 import com.inditex.rrhh.icmclcwb.model.app.tarea.mapper.TareaAmbitoGlobalFechaMapper;
 import com.inditex.rrhh.icmclcwb.model.app.tarea.service.TareaAmbitoGlobalFechaServiceImpl;
+import com.inditex.rrhh.icmclcwb.model.primary.tarea.entity.TareaAmbitoGlobalFecha;
 import com.inditex.rrhh.icmclcwb.model.primary.tarea.repository.TareaAmbitoGlobalFechaRepositoryCustom;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -35,11 +39,14 @@ class TareaAmbitoGlobalFechaServiceImplTest {
 
   private PeriodoDto periodoDto;
 
+  private TareaAmbitoGlobalFecha tareaAmbitoGlobalFecha;
+
   @BeforeEach
   void setUp() {
     this.tareaAmbitoGlobalFechaDto = new TareaAmbitoGlobalFechaDto();
     this.tareaDto = new TareaDto();
     this.periodoDto = new PeriodoDto();
+    this.tareaAmbitoGlobalFecha = new TareaAmbitoGlobalFecha();
   }
 
   @Test
@@ -54,4 +61,14 @@ class TareaAmbitoGlobalFechaServiceImplTest {
 
     verify(this.tareaAmbitoGlobalFechaRepositoryCustom).findFechaAmbitoDtoByIdTareaAndIdTipoDato(idTarea, idTipoDato);
   }
+
+  @Test
+    void saveTest() {
+        when(this.tareaAmbitoGlobalFechaMapper.tareaAmbitoGlobalFechaDtoToTareaAmbitoGlobalFecha(any(), any()))
+            .thenReturn(Collections.singletonList(this.tareaAmbitoGlobalFecha));
+
+      this.tareaAmbitoGlobalFechaService.save(this.tareaAmbitoGlobalFechaDto, this.tareaDto);
+
+        verify(this.tareaAmbitoGlobalFechaRepositoryCustom).save(any());
+    }
 }
