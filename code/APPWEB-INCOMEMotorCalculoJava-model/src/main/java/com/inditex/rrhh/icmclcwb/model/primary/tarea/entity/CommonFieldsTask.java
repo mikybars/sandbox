181422164faf01e1caf7725d77d0e29a1745f4e4
Alejandro Gallego <alejandrogmori@ext.inditex.com>
2025-01-07 17:48:23 +0100

    feat(INCOME-1319): separate common fields into other subclasses to avoid duplication

---
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/entity/CommonFieldsTask.java
@@ -0,0 +1,48 @@
+package com.inditex.rrhh.icmclcwb.model.primary.tarea.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.jspecify.annotations.NonNull;
+
+@EqualsAndHashCode
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonFieldsTask {
+
+  @NonNull
+  @OneToOne
+  @JoinColumn(name = "ID_TAREA", nullable = false)
+  private Tarea tarea;
+
+  @NonNull
+  @Column(name = "FECHA_INICIO", nullable = false)
+  @Temporal(TemporalType.DATE)
+  private Date fechaInicio;
+
+  @NonNull
+  @Column(name = "FECHA_FIN", nullable = false)
+  @Temporal(TemporalType.DATE)
+  private Date fechaFin;
+
+  @NotBlank
+  @Column(name = "CCL_ID_ORIGEN", nullable = false, length = 48)
+  private String cclIdOrigen;
+
+  @NotBlank
+  @Column(name = "STD_ID_LEG_ENT", nullable = false, length = 48)
+  private String stdIdLegEnt;
+
+}
