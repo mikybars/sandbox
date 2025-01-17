8fc36cc5fcd48319ae3a77d0848678d8249a5172
Xoel Otero Telmo <xoelot@ext.inditex.com>
2025-01-17 09:21:49 +0100

    [INCOME-658] revert changes Meta4ClientPoolConfig

--- code/APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/meta4/pool/Meta4ClientPoolConfig.java
+++ code/APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/meta4/pool/Meta4ClientPoolConfig.java
@@ -6,24 +6,25 @@ import com.inditex.rrhh.icmclcwb.model.meta4.pool.Meta4ClientAbstract;
 import com.inditex.rrhh.icmclcwb.model.meta4.pool.Meta4ClientFactory;
 import com.inditex.rrhh.icmclcwb.model.meta4.pool.Meta4ClientPool;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class Meta4ClientPoolConfig {
 
-  private final Meta4ClientConfigProperties meta4ClientConfigProperties;
+  @Autowired
+  @Qualifier("meta4ClientConfigProperties")
+  private Meta4ClientConfigProperties config;
 
   @Bean(name = "meta4ClientPool")
   public Meta4ClientPool meta4ClientPool(
       @Qualifier("meta4LoginClientFactory") final Meta4ClientAbstract<LoginService> meta4LoginClientFactory,
       @Qualifier("meta4IcmWsCalcIncomeClientFactory") final Meta4ClientAbstract<IcmWsCalcIncomeService> meta4IcmWsCalcIncomeClientFactory) {
-    Meta4ClientFactory meta4ClientFactory = new Meta4ClientFactory();
-    meta4ClientFactory.setMeta4ClientCredentials(meta4ClientConfigProperties.getCredentials());
-    meta4ClientFactory.setMeta4ClientProperties(meta4ClientConfigProperties.getPool());
+    final Meta4ClientFactory meta4ClientFactory = new Meta4ClientFactory();
+    meta4ClientFactory.setMeta4ClientCredentials(this.config.getCredentials());
+    meta4ClientFactory.setMeta4ClientProperties(this.config.getPool());
     meta4ClientFactory.setLoginServiceFactory(meta4LoginClientFactory);
     meta4ClientFactory.setIcmWsCalcIncomeServiceFactory(meta4IcmWsCalcIncomeClientFactory);
     return new Meta4ClientPool(meta4ClientFactory);
