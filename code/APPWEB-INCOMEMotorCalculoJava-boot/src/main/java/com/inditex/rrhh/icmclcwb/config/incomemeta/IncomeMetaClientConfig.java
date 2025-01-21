6726a2b66b7cc23555be1616ea346c8c971b177a
Xoel Otero Telmo <xoelot@ext.inditex.com>
2025-01-21 11:49:41 +0100

    [INCOME-1357] fix Qualifier

--- code/APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/incomemeta/IncomeMetaClientConfig.java
+++ code/APPWEB-INCOMEMotorCalculoJava-boot/src/main/java/com/inditex/rrhh/icmclcwb/config/incomemeta/IncomeMetaClientConfig.java
@@ -30,7 +30,7 @@ public class IncomeMetaClientConfig {
   }
 
   @Bean(name = "empleadosIncomeMetaApiClient")
-  public EmpleadosApi empleadosIcomeMetaApiClient(@Qualifier("empleadosMetaApiClient") final ApiClient apiClient) {
+  public EmpleadosApi empleadosIcomeMetaApiClient(@Qualifier("incomeMetaApiClient") final ApiClient apiClient) {
     return new EmpleadosApi(apiClient);
   }
 
