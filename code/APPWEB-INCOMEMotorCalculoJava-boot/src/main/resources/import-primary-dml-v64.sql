4c71dd544764fe1312bad046610e70299da3f863
Alex Gallego <alejandrogmori@ext.inditex.com>
2025-10-23 09:31:04 +0200

    feat(INCOME-2756): add new non-blocking grouped validation

---
+++ code/APPWEB-INCOMEMotorCalculoJava-boot/src/main/resources/import-primary-dml-v64.sql
@@ -0,0 +1,10 @@
+// CREACION NUEVA VALIDACION AGRUPADA NO BLOQUEANTE
+// Esta validación ejecuta en paralelo validarCalculoPendienteV1, validarImporteExcedidoV1 y validarPorcentaje0V1
+// y envía un único correo consolidado sin detener el flujo de cálculo
+
+INSERT INTO ACCION (ID_ACCION, NOMBRE, DESCRIPCION, REACCION_PESO, ES_REACCION_ESPERAR, ES_REACCION_REINTENTO, REACCION_REINTENTO_MAX, REACCION_REINTENTO_DELAY) VALUES (35, 'validacionesAgrupadasV1', 'Execute grouped non-blocking validations  and send consolidated email.', 100, 0, 0, 0, 0);
+
+INSERT INTO FASE_ACCION (ID_FASE, ID_ACCION, ID_PUNTO_EJECUCION, PESO) VALUES (4, 35, 3, 100);
+
+INSERT INTO ACCION_AMBITO (ID_ACCION, CCL_ID_ORIGEN, STD_ID_LEG_ENT, ES_ACTIVO) VALUES (35, 11, 11, 1);
+
