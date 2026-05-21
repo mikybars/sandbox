87b8031d5d071bc57ac3470f5e779034c7c30b94
Xoel Otero Telmo <xoelot@ext.inditex.com>
2026-05-21 18:01:21 +0200

    [INCOME-3322] fix format 3

--- code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoRepositoryCustom.java
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoRepositoryCustom.java
@@ -11,25 +11,25 @@ import jakarta.validation.constraints.NotNull;
 
 public interface TareaCalculoRepositoryCustom {
 
-    void regularizarMejorOpcion(@NotNull TareaDto tareaDto);
+  void regularizarMejorOpcion(@NotNull TareaDto tareaDto);
 
-    void regularizarChallenge(@NotNull TareaDto tareaDto,
-        @NotNull final IdPersonaLocalChallengeDto idPersonaLocalChallengeDto);
+  void regularizarChallenge(@NotNull TareaDto tareaDto,
+      @NotNull final IdPersonaLocalChallengeDto idPersonaLocalChallengeDto);
 
-    void regularizarMejorOpcionSinFechas(@NotNull final TareaDto tareaDto);
+  void regularizarMejorOpcionSinFechas(@NotNull final TareaDto tareaDto);
 
-    void regularizarMejorOpcionSinFechasTodoPeriodo(@NotNull final TareaDto tareaDto);
+  void regularizarMejorOpcionSinFechasTodoPeriodo(@NotNull final TareaDto tareaDto);
 
-    List<IdPersonaLocalDto> findPersonaImporteExcedidoByIdTarea(@NotNull Long idTarea, @NotNull String cclIdOrigen,
-        @NotNull String stdIdLegEnt);
+  List<IdPersonaLocalDto> findPersonaImporteExcedidoByIdTarea(@NotNull Long idTarea, @NotNull String cclIdOrigen,
+      @NotNull String stdIdLegEnt);
 
-    List<IdPersonaLocalDto> findPersonaCalculoPendiente(@NotNull Long idTarea);
+  List<IdPersonaLocalDto> findPersonaCalculoPendiente(@NotNull Long idTarea);
 
-    List<IdPersonaLocalDto> findPersonaPorcentaje0(@NotNull Long idTarea);
+  List<IdPersonaLocalDto> findPersonaPorcentaje0(@NotNull Long idTarea);
 
-    List<IdLocalizacionLocalDto> findTiendaVentasSinPresencias(@NotNull Long idTarea);
+  List<IdLocalizacionLocalDto> findTiendaVentasSinPresencias(@NotNull Long idTarea);
 
-    List<IdLocalizacionLocalDto> findTiendaPresenciasSinVentas(@NotNull Long idTarea);
+  List<IdLocalizacionLocalDto> findTiendaPresenciasSinVentas(@NotNull Long idTarea);
 
-    List<IdPersonaLocalDto> findPersonaPresenciasMismaFechaDistintaTienda(@NotNull Long idTarea);
+  List<IdPersonaLocalDto> findPersonaPresenciasMismaFechaDistintaTienda(@NotNull Long idTarea);
 }
