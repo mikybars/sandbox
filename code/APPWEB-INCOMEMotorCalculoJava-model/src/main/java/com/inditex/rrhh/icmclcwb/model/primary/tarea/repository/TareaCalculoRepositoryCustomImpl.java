effb24cddc3885b2c660daf731fa7e8a6d574799
Xoel Otero Telmo <xoelot@ext.inditex.com>
2026-05-21 17:40:03 +0200

    [INCOME-3322] new validation: employees with attendance records in multiple stores for the same date

--- code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoRepositoryCustomImpl.java
+++ code/APPWEB-INCOMEMotorCalculoJava-model/src/main/java/com/inditex/rrhh/icmclcwb/model/primary/tarea/repository/TareaCalculoRepositoryCustomImpl.java
@@ -20,146 +20,162 @@ import org.springframework.stereotype.Repository;
 public class TareaCalculoRepositoryCustomImpl extends JdbcBatchPrimaryRepositoryAbstract<TareaCalculo>
     implements TareaCalculoRepositoryCustom {
 
-  @Value("#{primaryQuery['TareaCalculoRepositoryCustom.regularizarMejorOpcion']}")
-  private String sqlRegularizarMejorOpcion;
+    @Value("#{primaryQuery['TareaCalculoRepositoryCustom.regularizarMejorOpcion']}")
+    private String sqlRegularizarMejorOpcion;
 
-  @Value("#{primaryQuery['TareaCalculoRepositoryCustom.regularizarMejorOpcionSinFechas']}")
-  private String sqlRegularizarMejorOpcionSinFechas;
+    @Value("#{primaryQuery['TareaCalculoRepositoryCustom.regularizarMejorOpcionSinFechas']}")
+    private String sqlRegularizarMejorOpcionSinFechas;
 
-  @Value("#{primaryQuery['TareaCalculoRepositoryCustom.regularizarMejorOpcionSinFechasTodoPeriodo']}")
-  private String sqlRegularizarMejorOpcionSinFechasTodoPeriodo;
+    @Value("#{primaryQuery['TareaCalculoRepositoryCustom.regularizarMejorOpcionSinFechasTodoPeriodo']}")
+    private String sqlRegularizarMejorOpcionSinFechasTodoPeriodo;
 
-  @Value("#{primaryQuery['TareaCalculoRepositoryCustom.regularizarChallenge']}")
-  private String sqlRegularizarChallenge;
+    @Value("#{primaryQuery['TareaCalculoRepositoryCustom.regularizarChallenge']}")
+    private String sqlRegularizarChallenge;
 
-  @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findPersonaImporteExcedidoByIdTarea']}")
-  private String sqlRecuperarPersonasImporteExcedido;
+    @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findPersonaImporteExcedidoByIdTarea']}")
+    private String sqlRecuperarPersonasImporteExcedido;
 
-  @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findPersonaCalculoPendiente']}")
-  private String sqlRecuperarPersonasCalculoPendiente;
+    @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findPersonaCalculoPendiente']}")
+    private String sqlRecuperarPersonasCalculoPendiente;
 
-  @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findPersonaPorcentaje0']}")
-  private String sqlRecuperarPersonasPorcentaje0;
+    @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findPersonaPorcentaje0']}")
+    private String sqlRecuperarPersonasPorcentaje0;
 
-  @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findVentasSinPresencias']}")
-  private String sqlRecuperarTiendasVentasSinPresencias;
+    @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findVentasSinPresencias']}")
+    private String sqlRecuperarTiendasVentasSinPresencias;
 
-  @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findPresenciasSinVentas']}")
-  private String sqlRecuperarTiendasPresenciasSinVentas;
+    @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findPresenciasSinVentas']}")
+    private String sqlRecuperarTiendasPresenciasSinVentas;
 
-  @Override
-  public void regularizarMejorOpcion(@NotNull final TareaDto tareaDto) {
-    final MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, tareaDto.getId());
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_FECHA_INICIO_PERIODO,
-        TimeUtils.toDate(tareaDto.getFechaInicioPeriodo()));
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ACTIVO, SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE);
-    this.update(this.sqlRegularizarMejorOpcion, params);
-  }
+    @Value("#{primaryQuery['TareaCalculoRepositoryCustom.findPersonaPresenciasMismaFechaDistintaTienda']}")
+    private String sqlRecuperarPersonasPresenciasMismaFechaDistintaTienda;
 
-  @Override
-  public void regularizarMejorOpcionSinFechas(@NotNull final TareaDto tareaDto) {
-    final MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, tareaDto.getId());
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_FECHA_INICIO_PERIODO,
-        TimeUtils.toDate(tareaDto.getFechaInicioPeriodo()));
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ACTIVO, SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE);
-    this.update(this.sqlRegularizarMejorOpcionSinFechas, params);
-  }
+    @Override
+    public void regularizarMejorOpcion(@NotNull final TareaDto tareaDto) {
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, tareaDto.getId());
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_FECHA_INICIO_PERIODO,
+            TimeUtils.toDate(tareaDto.getFechaInicioPeriodo()));
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ACTIVO, SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE);
+        this.update(this.sqlRegularizarMejorOpcion, params);
+    }
 
-  @Override
-  public void regularizarMejorOpcionSinFechasTodoPeriodo(@NotNull final TareaDto tareaDto) {
-    final MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, tareaDto.getId());
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_FECHA_INICIO_PERIODO,
-        TimeUtils.toDate(tareaDto.getFechaInicioPeriodo()));
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ACTIVO, SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE);
-    this.update(this.sqlRegularizarMejorOpcionSinFechasTodoPeriodo, params);
-  }
+    @Override
+    public void regularizarMejorOpcionSinFechas(@NotNull final TareaDto tareaDto) {
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, tareaDto.getId());
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_FECHA_INICIO_PERIODO,
+            TimeUtils.toDate(tareaDto.getFechaInicioPeriodo()));
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ACTIVO, SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE);
+        this.update(this.sqlRegularizarMejorOpcionSinFechas, params);
+    }
 
-  @Override
-  public void regularizarChallenge(@NotNull final TareaDto tareaDto,
-      @NotNull final IdPersonaLocalChallengeDto idPersonaLocalChallengeDto) {
-    final MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, tareaDto.getId());
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_CCL_ID_PERSON, idPersonaLocalChallengeDto.getCclIdPerson());
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_STD_OR_HR_PERIOD, idPersonaLocalChallengeDto.getStdOrHrPeriod());
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_IDS_TIPOS_CALCULO,
-        idPersonaLocalChallengeDto.getIcmIdTpCalculo());
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_IDS_TIPOS_COMISION,
-        idPersonaLocalChallengeDto.getIcmIdTpComision());
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ES_DESPLAZAMIENTO,
-        idPersonaLocalChallengeDto.getEsDesplazamiento());
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ES_DESPLAZAMIENTO_BASE,
-        idPersonaLocalChallengeDto.getEsDesplazamientoBase());
-    this.update(this.sqlRegularizarChallenge, params);
-  }
+    @Override
+    public void regularizarMejorOpcionSinFechasTodoPeriodo(@NotNull final TareaDto tareaDto) {
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, tareaDto.getId());
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_FECHA_INICIO_PERIODO,
+            TimeUtils.toDate(tareaDto.getFechaInicioPeriodo()));
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ACTIVO, SqlPrimaryConstants.SQL_VALUE_BOOLEAN_TRUE);
+        this.update(this.sqlRegularizarMejorOpcionSinFechasTodoPeriodo, params);
+    }
 
-  @Override
-  public List<IdPersonaLocalDto> findPersonaImporteExcedidoByIdTarea(
-      @NotNull Long idTarea, @NotNull String cclIdOrigen, @NotNull String stdIdLegEnt) {
-    final MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_CCL_ID_ORIGEN, cclIdOrigen);
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_STD_ID_LEG_ENT, stdIdLegEnt);
-    return this.query(
-        this.sqlRecuperarPersonasImporteExcedido,
-        params, (rs, rowNum) -> {
-          final IdPersonaLocalDto dto = new IdPersonaLocalDto();
-          dto.setIdPersonaLocal(rs.getString(SqlPrimaryConstants.SQL_RESULT_CCL_ID_PERSON));
-          return dto;
-        });
-  }
+    @Override
+    public void regularizarChallenge(@NotNull final TareaDto tareaDto,
+        @NotNull final IdPersonaLocalChallengeDto idPersonaLocalChallengeDto) {
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, tareaDto.getId());
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_CCL_ID_PERSON, idPersonaLocalChallengeDto.getCclIdPerson());
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_STD_OR_HR_PERIOD, idPersonaLocalChallengeDto.getStdOrHrPeriod());
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_IDS_TIPOS_CALCULO,
+            idPersonaLocalChallengeDto.getIcmIdTpCalculo());
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_IDS_TIPOS_COMISION,
+            idPersonaLocalChallengeDto.getIcmIdTpComision());
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ES_DESPLAZAMIENTO,
+            idPersonaLocalChallengeDto.getEsDesplazamiento());
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ES_DESPLAZAMIENTO_BASE,
+            idPersonaLocalChallengeDto.getEsDesplazamientoBase());
+        this.update(this.sqlRegularizarChallenge, params);
+    }
 
-  @Override
-  public List<IdPersonaLocalDto> findPersonaCalculoPendiente(Long idTarea) {
-    final MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
-    return this.query(
-        this.sqlRecuperarPersonasCalculoPendiente,
-        params, (rs, rowNum) -> {
-          final IdPersonaLocalDto dto = new IdPersonaLocalDto();
-          dto.setIdPersonaLocal(rs.getString(SqlPrimaryConstants.SQL_RESULT_CCL_ID_PERSON));
-          return dto;
-        });
-  }
+    @Override
+    public List<IdPersonaLocalDto> findPersonaImporteExcedidoByIdTarea(
+        @NotNull Long idTarea, @NotNull String cclIdOrigen, @NotNull String stdIdLegEnt) {
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_CCL_ID_ORIGEN, cclIdOrigen);
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_STD_ID_LEG_ENT, stdIdLegEnt);
+        return this.query(
+            this.sqlRecuperarPersonasImporteExcedido,
+            params, (rs, rowNum) -> {
+                final IdPersonaLocalDto dto = new IdPersonaLocalDto();
+                dto.setIdPersonaLocal(rs.getString(SqlPrimaryConstants.SQL_RESULT_CCL_ID_PERSON));
+                return dto;
+            });
+    }
 
-  @Override
-  public List<IdPersonaLocalDto> findPersonaPorcentaje0(Long idTarea) {
-    final MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
-    return this.query(
-        this.sqlRecuperarPersonasPorcentaje0,
-        params, (rs, rowNum) -> {
-          final IdPersonaLocalDto dto = new IdPersonaLocalDto();
-          dto.setIdPersonaLocal(rs.getString(SqlPrimaryConstants.SQL_RESULT_CCL_ID_PERSON));
-          return dto;
-        });
-  }
+    @Override
+    public List<IdPersonaLocalDto> findPersonaCalculoPendiente(Long idTarea) {
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
+        return this.query(
+            this.sqlRecuperarPersonasCalculoPendiente,
+            params, (rs, rowNum) -> {
+                final IdPersonaLocalDto dto = new IdPersonaLocalDto();
+                dto.setIdPersonaLocal(rs.getString(SqlPrimaryConstants.SQL_RESULT_CCL_ID_PERSON));
+                return dto;
+            });
+    }
 
-  @Override
-  public List<IdLocalizacionLocalDto> findTiendaVentasSinPresencias(Long idTarea) {
-    final MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
-    return this.query(
-        this.sqlRecuperarTiendasVentasSinPresencias,
-        params, (rs, rowNum) -> {
-          final IdLocalizacionLocalDto dto = new IdLocalizacionLocalDto();
-          dto.setId(rs.getString(SqlPrimaryConstants.SQL_RESULT_ID_LOCALIZACION_LOCAL));
-          return dto;
-        });
-  }
+    @Override
+    public List<IdPersonaLocalDto> findPersonaPorcentaje0(Long idTarea) {
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
+        return this.query(
+            this.sqlRecuperarPersonasPorcentaje0,
+            params, (rs, rowNum) -> {
+                final IdPersonaLocalDto dto = new IdPersonaLocalDto();
+                dto.setIdPersonaLocal(rs.getString(SqlPrimaryConstants.SQL_RESULT_CCL_ID_PERSON));
+                return dto;
+            });
+    }
 
-  @Override
-  public List<IdLocalizacionLocalDto> findTiendaPresenciasSinVentas(Long idTarea) {
-    final MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
-    return this.query(
-        this.sqlRecuperarTiendasPresenciasSinVentas,
-        params, (rs, rowNum) -> {
-          final IdLocalizacionLocalDto dto = new IdLocalizacionLocalDto();
-          dto.setId(rs.getString(SqlPrimaryConstants.SQL_RESULT_ID_LOCALIZACION_LOCAL));
-          return dto;
-        });
-  }
+    @Override
+    public List<IdLocalizacionLocalDto> findTiendaVentasSinPresencias(Long idTarea) {
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
+        return this.query(
+            this.sqlRecuperarTiendasVentasSinPresencias,
+            params, (rs, rowNum) -> {
+                final IdLocalizacionLocalDto dto = new IdLocalizacionLocalDto();
+                dto.setId(rs.getString(SqlPrimaryConstants.SQL_RESULT_ID_LOCALIZACION_LOCAL));
+                return dto;
+            });
+    }
+
+    @Override
+    public List<IdLocalizacionLocalDto> findTiendaPresenciasSinVentas(Long idTarea) {
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
+        return this.query(
+            this.sqlRecuperarTiendasPresenciasSinVentas,
+            params, (rs, rowNum) -> {
+                final IdLocalizacionLocalDto dto = new IdLocalizacionLocalDto();
+                dto.setId(rs.getString(SqlPrimaryConstants.SQL_RESULT_ID_LOCALIZACION_LOCAL));
+                return dto;
+            });
+    }
+
+    @Override
+    public List<IdPersonaLocalDto> findPersonaPresenciasMismaFechaDistintaTienda(Long idTarea) {
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue(SqlPrimaryConstants.SQL_PARAM_ID_TAREA, idTarea);
+        return this.query(
+            this.sqlRecuperarPersonasPresenciasMismaFechaDistintaTienda,
+            params, (rs, rowNum) -> {
+                final IdPersonaLocalDto dto = new IdPersonaLocalDto();
+                dto.setIdPersonaLocal(rs.getString(SqlPrimaryConstants.SQL_RESULT_CCL_ID_PERSON));
+                return dto;
+            });
+    }
 }
